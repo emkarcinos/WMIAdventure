872290e00eadb15168155410213574efb4326992
Mateusz <wirus006@gmail.com>
2021-04-20 22:52:31 +0200

    initialize frontend via create-react-app starter

---
+++ WMIAdventure/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="App__header">
+        Hello Wmi-Adventure!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
