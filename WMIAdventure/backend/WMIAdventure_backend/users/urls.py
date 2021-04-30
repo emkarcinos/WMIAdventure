8551e2642813675cf2c802fe5fae47d7d2b039f9
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-30 12:58:32 +0200

    Add url path to view serving list of users in backend users app #81

--- WMIAdventure/backend/WMIAdventure_backend/users/urls.py
+++ WMIAdventure/backend/WMIAdventure_backend/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserRegister
+from .views import UserRegister, UserList
 
 urlpatterns = [
+    path('', UserList.as_view(), name='users-list'),
     path('register/', UserRegister.as_view(), name='users-register')
 ]
