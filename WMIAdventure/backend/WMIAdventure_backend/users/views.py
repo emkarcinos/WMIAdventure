4f2649ec45c77f46d2d4de739a3ccc117559e133
Michael-Czekanski <michal.cz2202@gmail.com>
2021-04-30 12:58:00 +0200

    Add view displaying list of users in backend users app #81

--- WMIAdventure/backend/WMIAdventure_backend/users/views.py
+++ WMIAdventure/backend/WMIAdventure_backend/users/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from .models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, BasicUserInfoSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -19,3 +19,14 @@ class UserRegister(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserList(APIView):
+
+    def get(self, request):
+        """
+        List users.
+        """
+
+        serializer = BasicUserInfoSerializer(User.objects.all(), many=True)
+        return Response(serializer.data)
