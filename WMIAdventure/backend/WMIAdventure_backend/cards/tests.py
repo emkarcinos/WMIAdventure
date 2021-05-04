ab55d5fa94d527d44be25f3920aa220b3e0562b0
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-04 16:04:10 +0200

    Add CardLevelSerializer tests in cards app in backend #128

--- WMIAdventure/backend/WMIAdventure_backend/cards/tests.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from .models import CardLevel
 from django.db.utils import IntegrityError
+from .models import CardLevel
+from .serializers import CardLevelSerializer
 
 
 class CardLevelTestCase(TestCase):
@@ -23,3 +24,39 @@ class CardLevelTestCase(TestCase):
         CardLevel.objects.create(level=1)
 
         self.assertRaises(IntegrityError, CardLevel.objects.create, level=1)
+
+
+class CardLevelSerializerTestCase(TestCase):
+    def setUp(self) -> None:
+        self.cLvlEnum = CardLevel.Level(1)
+        self.cLvlVal = self.cLvlEnum.value
+        self.cLvlLabel = str(self.cLvlEnum.label)
+
+    def testSerialization(self):
+        CardLevel.objects.create(level=self.cLvlEnum)
+        cLvl = CardLevel.objects.get(level=self.cLvlVal)
+
+        serializer = CardLevelSerializer(instance=cLvl)
+
+        actualLevel = serializer.data.get("level")
+        actualName = serializer.data.get("name")
+
+        self.assertEquals(actualLevel, cLvl.level)
+        self.assertEquals(actualName, cLvl.name)
+
+    def testDeserialization(self):
+        data = {"level": self.cLvlVal, "name": self.cLvlLabel}
+
+        serializer = CardLevelSerializer(data=data)
+
+        try:
+            self.assertTrue(serializer.is_valid())
+        except AssertionError as e:
+            print(serializer.errors)
+            raise e
+
+        cLvl = serializer.save()
+
+        self.assertEquals(cLvl.level, self.cLvlVal)
+        self.assertEquals(cLvl.name, self.cLvlLabel)
+
