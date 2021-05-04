6847ad82d806c1da53247d5e8106cd11fe20d9cf
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-04 15:07:48 +0200

    Add CardLevel cls tests in cards app in backend #128

--- WMIAdventure/backend/WMIAdventure_backend/cards/tests.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import CardLevel
+from django.db.utils import IntegrityError
 
-# Create your tests here.
+
+class CardLevelTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_name_after_creation(self):
+        CardLevel.objects.create(level=CardLevel.Level(1))
+        c_lvl = CardLevel.objects.get(level=1)
+
+        self.assertEqual(c_lvl.name, CardLevel.Level(1).label)
+
+    def test_creation_with_int(self):
+        c_lvl = CardLevel.objects.create(level=1)
+
+        self.assertIsInstance(c_lvl.level, CardLevel.Level)
+        self.assertEqual(c_lvl.name, CardLevel.Level(1).label)
+
+    def test_creation_duplicates(self):
+        CardLevel.objects.create(level=1)
+
+        self.assertRaises(IntegrityError, CardLevel.objects.create, level=1)
