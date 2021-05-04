449d013490298aaa911b23d6ac60616ff6221bfc
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-04 16:12:43 +0200

    Add view listing all CardLevels in cards app in backend #128

--- WMIAdventure/backend/WMIAdventure_backend/cards/views.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from .models import CardLevel
+from .serializers import CardLevelSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class CardLevelList(APIView):
+
+    def get(self, request):
+        """
+        List of Card Levels.
+        """
+
+        serializer = CardLevelSerializer(CardLevel.objects.all(), many=True)
+        return Response(serializer.data)
