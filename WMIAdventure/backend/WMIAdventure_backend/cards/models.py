f0ce3d0e6e69e71e400fdb4e0c0cae72dade7290
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-04 14:45:10 +0200

    Add CardLevel model cls in cards app in backend #128

--- WMIAdventure/backend/WMIAdventure_backend/cards/models.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class CardLevel(models.Model):
+    """
+    Card Level.
+    """
+
+    class Level(models.IntegerChoices):
+        """
+        Enum of possible card levels.
+        """
+        COMMON = 1, _("Common")
+
+    level = models.IntegerField(primary_key=True, choices=Level.choices, default=Level.COMMON)
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    def save(self, *args, **kwargs):
+        if not isinstance(self.level, CardLevel.Level):
+            self.level = CardLevel.Level(self.level)
+        self.name = self.level.label
+
+        super(CardLevel, self).save(*args, **kwargs)
