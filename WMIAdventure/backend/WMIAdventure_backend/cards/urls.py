1aaf265089d41b08e089429f1609353eac168db9
Michał Czekański <43492764+Michael-Czekanski@users.noreply.github.com>
2021-05-04 22:41:00 +0200

    Add ending "/" in url path with in backend

--- WMIAdventure/backend/WMIAdventure_backend/cards/urls.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/urls.py
@@ -3,7 +3,7 @@ from .views import CardEffectList, CardEffectObjectView, CardLevelList, CardLeve
 
 urlpatterns = [
     path('card-effect/', CardEffectList.as_view(), name='card-effect'),
-    path('card-effect/<int:pk>', CardEffectObjectView.as_view()),
+    path('card-effect/<int:pk>/', CardEffectObjectView.as_view()),
     path('card-level/', CardLevelList.as_view(), name='cards-levels-list'),
     path('card-level/<int:pk>/', CardLevelDetail.as_view(), name='cards-level-details')
 ]
