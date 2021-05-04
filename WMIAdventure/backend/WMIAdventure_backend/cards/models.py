0af4737bfed74fcfa8b3f13eaf019b6a20927ada
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-04 17:59:32 +0200

    Added CardEffect model #128

--- WMIAdventure/backend/WMIAdventure_backend/cards/models.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class CardEffect(models.Model):
+    """
+    Represents a card effect, contains it's identifier, name and displayed tooltip.
+    """
+
+    class EffectId(models.IntegerChoices):
+        """
+        All possible effects
+        """
+        DMG = 1, _("Zadawanie obrażeń")
+        SHIELD = 2, _("Tarcza")
+        SWAP_RND = 3, _("Losowa zamiana kolejności kart")
+        STOP = 4, _("Zatrzymanie na jedną turę")
+        DOUBLEACTION = 5, _("Dwukrotne wykonanie się karty")
+        HEAL = 6, _("Leczenie")
+        BLOCK = 7, _("Blokowanie następnej karty")
+        EMPOWER = 8, _("Zwiększenie mocy następnej karty")
+        SKIP = 9, _("Pomijanie następnej karty")
+
+    id = models.IntegerField(primary_key=True, choices=EffectId.choices, default=EffectId.DMG)
+    name = models.CharField(max_length=50)
+    tooltip = models.TextField(max_length=150, null=True)
+
+    def __str__(self):
+        return f"{self.name}"
--- WMIAdventure/backend/WMIAdventure_backend/cards/tests.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/tests.py
@@ -1,3 +1,38 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
 # Create your tests here.
+from .models import CardEffect
+
+
+class CardEffectTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_database_populated(self):
+        """
+        I'm not sure whether this test should exist.
+        The database needs to be populated before, but this is more of an integration test rather than unit test.
+        I'd rather say this tests the migration, not the model itself.
+        """
+
+        # All objects
+        entries = CardEffect.objects.all()
+
+        # Gets data from enum class
+        enum_vals = CardEffect.EffectId.values
+        enum_labels = CardEffect.EffectId.labels
+
+        for i in range(1, len(entries)):
+            self.assertEqual(entries[i].id, enum_vals[i])
+            self.assertEqual(entries[i].name, enum_labels[i])
+
+    def test_duplicates(self):
+        """
+        Checks whether you cannot make two models with the same ID
+        """
+        test_id = 90
+        CardEffect.objects.create(id=test_id)
+        self.assertRaises(IntegrityError,
+        CardEffect.objects.create, id=test_id)
+
