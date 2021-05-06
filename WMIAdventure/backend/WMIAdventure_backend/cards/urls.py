82b5990cfe19dd8270438c276e86e37451458a87
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-06 14:44:06 +0200

    Added all cards view #128

--- WMIAdventure/backend/WMIAdventure_backend/cards/urls.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CardEffectList, CardEffectObjectView, CardLevelList, CardLevelDetail
+from .views import *
 
 urlpatterns = [
     path('card-effect/', CardEffectList.as_view(), name='card-effect'),
     path('card-effect/<int:pk>/', CardEffectObjectView.as_view()),
     path('card-level/', CardLevelList.as_view(), name='cards-levels-list'),
-    path('card-level/<int:pk>/', CardLevelDetail.as_view(), name='cards-level-details')
+    path('card-level/<int:pk>/', CardLevelDetail.as_view(), name='cards-level-details'),
+    path('all/', CardsView.as_view(), name='all-cards')
 ]
--- WMIAdventure/backend/WMIAdventure_backend/cards/views.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/views.py
@@ -1,7 +1,5 @@
-from .models import CardEffect
-from .serializers import CardEffectSerializer
-from .models import CardLevel
-from .serializers import CardLevelSerializer
+from .serializers import *
+from .models import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
@@ -50,3 +48,44 @@ class CardEffectObjectView(APIView):
 
         serializer = CardEffectSerializer(item)
         return Response(serializer.data)
+
+
+class CardsView(APIView):
+    """
+    All about cards.
+    """
+
+    def get(self, request, pk=None):
+        objects = []
+        try:
+            # Get all card info's
+            all_infos = list(CardInfo.objects.all())
+
+            for info in all_infos:
+                # Get cards with a given info
+                cards = list(Card.objects.filter(info=info))
+                cards_effects = []
+                for card in cards:
+                    # Get all effects for a given card
+                    for e in list(CardLevelEffects.objects.filter(card=card)):
+                        cards_effects.append(e)
+                objects.append(WholeCardSerializer.translate_models(info, cards, cards_effects))
+
+            serializer = WholeCardSerializer(objects, many=True)
+            return Response(serializer.data)
+        except CardInfo.DoesNotExist or CardLevelEffects.DoesNotExist or Card.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request):
+        card = request.data
+        serializer = WholeCardSerializer(data=card)
+        if not serializer.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class CardSingleView(APIView):
+    """
+    Single card views.
+    """
