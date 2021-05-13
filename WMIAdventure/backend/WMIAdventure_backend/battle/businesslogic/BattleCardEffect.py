ba52276e65e35c7c7b9315ff5fdb630c61b8f829
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-13 21:58:22 +0200

    Add ctor for BattleCardEffect cls in backend #138

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/BattleCardEffect.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/BattleCardEffect.py
@@ -1,2 +1,13 @@
+from WMIAdventure_backend.cards.models import CardLevelEffects
+
+
 class BattleCardEffect:
-    pass
+    def __init__(self, effect_model: CardLevelEffects):
+        """
+        Creates BattleCardEffect instance.
+        @param effect_model: Database model.
+        """
+
+        self.target = effect_model.target
+        self.power = effect_model.power
+        self.range = effect_model.range
