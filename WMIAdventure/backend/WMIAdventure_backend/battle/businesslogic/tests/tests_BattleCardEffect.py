c54b55fb0c62f8877245655d9f4e4915939ea236
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-14 00:36:12 +0200

    Add empty test cls for BattleCardEffect in backend #138

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_BattleCardEffect.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from WMIAdventure_backend.cards.models import CardLevelEffects
+from ..BattleCardEffect import BattleCardEffect
+
+
+class BattleCardEffectTestCase(TestCase):
+    pass
