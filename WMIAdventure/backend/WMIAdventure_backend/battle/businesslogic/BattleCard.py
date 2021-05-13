47cd5cdd84440c38c3c05ad16836ffb1f893c521
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-14 00:42:58 +0200

    Add BattleCard ctor in backend #138

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/BattleCard.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/BattleCard.py
@@ -1,6 +1,21 @@
+from typing import List
+
+from WMIAdventure_backend.cards.models import Card
+from .BattleCardEffect import BattleCardEffect
+
+
 class BattleCard:
     """
 
     """
+    effects: List[BattleCardEffect]
 
-    pass
+    def __init__(self, card_model: Card):
+        """
+        Creates BattleCard instance.
+        @param card_model: Database model.
+        """
+
+        self.effects = []
+        for effect_model in card_model.effects.all():
+            self.effects.append(BattleCardEffect(effect_model))
