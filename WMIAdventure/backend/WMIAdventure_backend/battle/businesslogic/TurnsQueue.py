e06a51c2dd45270f0919974fe39850aa5b0a3f35
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-14 10:47:39 +0200

    Implemented TurnsQueue class #138

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/TurnsQueue.py
@@ -0,0 +1,29 @@
+from .BattlePlayer import BattlePlayer
+from collections import deque
+
+
+class TurnsQueue:
+    """
+    Manages players turns.
+    """
+    queue = deque()
+
+    def __init__(self, attacker: BattlePlayer, defender: BattlePlayer):
+        self.queue.append(attacker)
+        self.queue.append(defender)
+
+    def turn(self):
+        """
+        Gets a player which is at the front of the queue, dequeues it, and puts it to the end of it.
+        """
+
+        player = self.queue.popleft()
+        self.queue.append(player)
+
+        return player
+
+    def enqueue(self, player: BattlePlayer):
+        """
+        Add a player to the end of the queue.
+        """
+        self.queue.append(player)
