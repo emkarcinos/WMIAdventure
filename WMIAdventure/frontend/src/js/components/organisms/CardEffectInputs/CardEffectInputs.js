c01fe45e4356dddda954c4d238df14efdbf5c113
Mateusz <wirus006@gmail.com>
2021-05-14 22:04:08 +0200

    remove node-sass and get effects data from API #142 #89

--- WMIAdventure/frontend/src/js/components/organisms/CardEffectInputs/CardEffectInputs.js
+++ WMIAdventure/frontend/src/js/components/organisms/CardEffectInputs/CardEffectInputs.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import StyledFieldset from './StyledFieldset';
 import CardEffectCheckbox from '../../molecules/CardEffectCheckbox';
-import effectsData from './effectsData';
 
 function CardEffectInputs() {
+    const [effects, setEffects] = React.useState([]);
+    const API = process.env['REACT_APP_API_URL'];
+
+    React.useEffect(() => {
+        fetch(`http://${API}/api/cards/card-effect/`)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => setEffects(data))
+            .catch(error => console.log(error));
+    }, [API]);
+
     return (
         <StyledFieldset>
             <legend>
                 Efekty karty
             </legend>
             {
-                effectsData.map((elem) => {
+                effects.map((elem) => {
                     return (
-                        <CardEffectCheckbox key={`card-effect-${elem.id}`} name={elem.name} label={elem.label} power={elem.power} />
+                        <CardEffectCheckbox key={`card-effect-${elem.id}`} name={elem.name} label={elem.name} />
                     );
                 })
             }
