bb4a03c1ffa8911976534d6ab89e170d3a9b4104
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-14 11:28:17 +0200

    Implemented BattleOutcome class #138

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/BattlePlayer.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/BattlePlayer.py
@@ -4,3 +4,6 @@ class BattlePlayer:
     It gets populated with model data.
     Has statistics that represent its current state and its card deck.
     """
+
+    def get_hp(self):
+        return 10.0
