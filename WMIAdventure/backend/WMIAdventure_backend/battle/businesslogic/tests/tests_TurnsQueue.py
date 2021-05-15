bbefcca3aefcdcb78e7348c5eadaf34de1526c0f
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-15 10:24:51 +0200

    Fixed turnsQueue unittest #138

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_TurnsQueue.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_TurnsQueue.py
@@ -1,13 +1,19 @@
-from unittest import TestCase
+from django.test import TestCase
 
-from IngameUsers.models import UserProfile
+from IngameUsers.models import UserProfile, Deck, UserCard, UserDeck
+from cards.models import CardInfo, Card, CardLevel, CardLevelEffects, CardEffect
 from users.models import User
 from ..BattlePlayer import BattlePlayer
+from ..BattlePlayerFactory import BattlePlayerFactory
 from ..TurnsQueue import TurnsQueue
 
 
 class TurnsQueueTestCase(TestCase):
-    def setUp(self) -> None:
+    user_profile_model1: UserProfile
+    user_profile_model2: UserProfile
+
+    @classmethod
+    def setUpTestData(cls):
         User.objects.all().delete()
         user_model1 = User.objects.create(
             username="user1",
@@ -19,16 +25,96 @@ class TurnsQueueTestCase(TestCase):
             email="user2@company.com"
         )
 
-        user_profile_model1 = UserProfile(user=user_model1, displayedUsername="user1")
-        user_profile_model2 = UserProfile(user=user_model2, displayedUsername="user2")
+        cls.user_profile_model1 = UserProfile(user=user_model1, displayedUsername="user1")
+        cls.user_profile_model2 = UserProfile(user=user_model2, displayedUsername="user2")
+        cls.user_profile_model1.save()
+        cls.user_profile_model2.save()
+        # Creating 5 test cards
+        cards = []
+        for i in range(5):
+            card_info = CardInfo.objects.create(
+                name=str(i),
+                tooltip="Tooltip",
+                image=None,
+                subject=None
+            )
+            card_info.save()
 
-        self.player1 = BattlePlayer(user_profile_model1)
-        self.player2 = BattlePlayer(user_profile_model2)
+            card = Card.objects.create(
+                info=card_info,
+                level=CardLevel.objects.get(pk=1),
+                next_level_cost=None
+            )
+
+            effect = CardLevelEffects.objects.create(card=card,
+                                                     card_effect=CardEffect(CardEffect.EffectId.DMG),
+                                                     target=CardLevelEffects.Target.OPPONENT,
+                                                     power=1.0,
+                                                     range=1.0)
+            effect.save()
+            card.save()
+            cards.append(card)
+
+        # Decks used by Player 1
+        p1_attacker_deck = Deck()
+        p1_defender_deck = Deck()
+
+        # Decks used by Player 2
+        p2_attacker_deck = Deck()
+        p2_defender_deck = Deck()
+
+        for i in range(len(cards)):
+            # Creating a card that belongs to P1
+            p1_card = UserCard.objects.create(user_profile=cls.user_profile_model1,
+                                              card=cards[i])
+            # The same card also belongs to P2
+            p2_card = UserCard.objects.create(user_profile=cls.user_profile_model2,
+                                              card=cards[i])
+            p1_card.save()
+            p2_card.save()
+            # Sorry for this eval function call, this will fail if we modified field names in Deck model
+            # Adding p1's cards to each deck (attacker and defender decks are the same here)
+            exec("p1_attacker_deck.card{} = p1_card".format(i + 1))
+            exec("p1_defender_deck.card{} = p1_card".format(i + 1))
+            # Same for p2
+            exec("p2_attacker_deck.card{} = p2_card".format(i + 1))
+            exec("p2_defender_deck.card{} = p2_card".format(i + 1))
+
+        p1_attacker_deck.save()
+        p1_defender_deck.save()
+
+        # Decks used by Player 2
+        p2_attacker_deck.save()
+        p2_defender_deck.save()
+
+        user1_deck1 = UserDeck(deck_number=1,
+                               deck=p1_defender_deck,
+                               user_profile=cls.user_profile_model1)
+        user1_deck1.save()
+        user1_deck2 = UserDeck(deck_number=2,
+                               deck=p1_attacker_deck,
+                               user_profile=cls.user_profile_model1)
+        user1_deck2.save()
+        user2_deck1 = UserDeck(deck_number=1,
+                               deck=p2_defender_deck,
+                               user_profile=cls.user_profile_model2)
+        user2_deck1.save()
+        user2_deck2 = UserDeck(deck_number=2,
+                               deck=p2_attacker_deck,
+                               user_profile=cls.user_profile_model2)
+        user2_deck2.save()
+
+    def setUp(self) -> None:
+        factory = BattlePlayerFactory.get_instance()
+        self.player1 = factory.create(user_profile_model=self.user_profile_model1,
+                                      is_attacker=True)
+        self.player2 = factory.create(user_profile_model=self.user_profile_model2,
+                                      is_attacker=False)
         self.queue = TurnsQueue(self.player1, self.player2)
 
     def test_queue_looping(self):
         iterations = 5
-        for _ in range(5):
+        for _ in range(iterations):
             first_player = self.queue.turn()
             self.assertEqual(first_player, self.player1)
 
@@ -45,4 +131,3 @@ class TurnsQueueTestCase(TestCase):
 
         third_player = self.queue.turn()
         self.assertEqual(third_player, self.player2)
-
