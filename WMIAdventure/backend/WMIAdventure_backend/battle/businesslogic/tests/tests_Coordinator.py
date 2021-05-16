9b2a8d0ef0c68f5e55dfe6eb24813d9d5847331d
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-16 15:12:17 +0200

    Coordinator unit tests #138

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_Coordinator.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+from battle.businesslogic.BattlePlayerFactory import BattlePlayerFactory
+from battle.businesslogic.Coordinator import Coordinator
+from battle.businesslogic.tests.Creator import Creator
+
+
+class CoordinatorTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.creator = Creator()
+        cls.profiles = cls.creator.get_user_profile_models()
+        cls.attacker = BattlePlayerFactory.get_instance().create(cls.profiles[0])
+        cls.defender = BattlePlayerFactory.get_instance().create(cls.profiles[1])
+        cls.coordinator = Coordinator(cls.attacker, cls.defender)
+
+    def test_next_turn(self):
+        self.coordinator.next_turn()
+
+    def test_opponent(self):
+        attacker = self.coordinator.attacker
+        defender = self.coordinator.defender
+
+        self.assertEqual(attacker, self.coordinator.get_players_opponent(defender))
+    @classmethod
+    def tearDownClass(cls):
+        cls.creator.perform_deletion()
