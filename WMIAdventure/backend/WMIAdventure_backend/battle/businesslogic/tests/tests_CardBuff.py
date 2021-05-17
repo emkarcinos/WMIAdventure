5e8890bd1cd65b6ed3496f5d9c2da97c0b38ccdd
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-17 21:32:20 +0200

    CardBuff tests #138

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_CardBuff.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from battle.businesslogic.CardBuff import CardBuff
+
+
+class CardBuffTestCase(TestCase):
+
+    def test_buff_delayed_activation(self):
+        delay = 2
+        buff = CardBuff(activation_delay_turns=delay, active_turns=999)
+        self.assertFalse(buff.can_trigger())
+        for _ in range(delay):
+            buff.update()
+
+        self.assertTrue(buff.can_trigger())
+
+    def test_buff_expiration(self):
+        timeout = 5
+        buff = CardBuff(active_turns=timeout)
+        self.assertTrue(buff.can_trigger())
+        for _ in range(timeout):
+            buff.update()
+
+        self.assertFalse(buff.can_trigger())
