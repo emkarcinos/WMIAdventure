d40ea8968bac0923cd2cd4e6b9d7fdd074bf1bf9
Mateusz <wirus006@gmail.com>
2021-05-18 23:21:35 +0200

    CardForm: transport useInput hook to outsite file #173

--- WMIAdventure/frontend/src/js/components/creative/organisms/CardForm/CardForm.js
+++ WMIAdventure/frontend/src/js/components/creative/organisms/CardForm/CardForm.js
@@ -1,40 +1,7 @@
 import React from 'react';
 import StyledForm from './StyledForm';
 import Label from '../../atoms/CardPowerInput/StyledFieldset/Label';
-
-function useInput(initialValue) {
-    const [value, setValue] = React.useState(initialValue);
-
-    const handleChange = (event) => {
-        if(event.target.name === 'level' || event.target.name === 'card_effect') {
-            if(event.target.checked) {
-                let newList = value.slice();
-                if(event.target.name === 'level')
-                    newList[Number(event.target.id[0]) - 1] = event.target.value;
-                else if(event.target.name)
-                    newList[Number(event.target.id[0]) - 1][Number(event.target.id[2]) - 1] = event.target.value;
-                setValue(newList);
-            } else {
-                let newList = value.slice();
-                if(event.target.name === 'level')
-                    newList[Number(event.target.id[0]) - 1] = undefined;
-                else if(event.target.name)
-                    newList[Number(event.target.id[0]) - 1][Number(event.target.id[2]) - 1] = undefined;
-                setValue(newList);
-            }
-        } else if(event.target.type === 'text' || event.target.name === 'tooltip') {
-            setValue(event.target.value);
-        } else {
-            let newList = value.slice();
-            if(event.target.name === 'next_level_cost')
-                newList[Number(event.target.id[0]) - 1] = event.target.value;
-            else newList[Number(event.target.id[0]) - 1][Number(event.target.id[2]) - 1] = event.target.value;
-            setValue(newList);
-        }
-    };
-
-    return [value, handleChange];
-}
+import useInput from './useInput';
 
 function CardForm() {
     const API = process.env['REACT_APP_API_URL'];
