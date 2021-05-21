2cb2bb69cb5b09c867acc084526ca5c4d50ac8b7
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-21 16:34:36 +0200

    Initialized recorder module #167

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/recorder/ProcessRecorder.py
@@ -0,0 +1,13 @@
+from battle.businesslogic.Player import Player
+
+
+class ProcessRecorder:
+    """
+    Registers every turn that has happened in a single Battle.
+    """
+
+    def __init__(self, attacker: Player, defender: Player):
+        pass
+
+    def record_turn(self, executor: Player, target: Player):
+        pass
