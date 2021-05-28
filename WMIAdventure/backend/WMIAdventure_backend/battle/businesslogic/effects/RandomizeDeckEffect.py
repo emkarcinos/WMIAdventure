5afa3f2ffc9985816c8ae64394dfe36ca1e7152a
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-28 11:18:35 +0200

    Fix RandomizeDeckEffect #131

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/RandomizeDeckEffect.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/RandomizeDeckEffect.py
@@ -5,14 +5,20 @@ import random
 class RandomizeDeckEffect(Effect):
     """
     Randomizes order of cards in deck.
+    This effect randomizes cards order til it's changed, there is no randomization which does nothing.
     """
 
     def on_activation(self, target, turns_queue):
         deck = target.deck
 
-        cards_order = [None for _ in range(deck.size())]
-        for i in range(deck.size()):
-            cards_order[i] = deck.lookup(i)
+        original_cards_order = [i for i in range(deck.size())]
+        new_cards_order = [i for i in range(deck.size())]
 
-        random.shuffle(cards_order)
-        deck.create_cards_queue(tuple(cards_order))
+        # Randomize cards order til it's changed.
+        while new_cards_order == original_cards_order:
+            random.shuffle(new_cards_order)
+
+        # Create tuple of BattleCard objects in new order
+        cards_in_new_order = tuple([deck.lookup(ind) for ind in new_cards_order])
+
+        deck.create_cards_queue(cards_in_new_order)
