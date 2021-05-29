71c74a43e8381f36fe8377d87426e45cdaebf5e5
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-30 00:37:39 +0200

    Add CardState class #135

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/card_states/CardState.py
@@ -0,0 +1,38 @@
+from typing import Any
+
+
+class CardState:
+    """
+    This is abstract class of card's state. Concrete card state classes should derive from this class.
+    Card can have states like being blocked.
+    """
+    turns_active: int
+    active: bool
+
+    def __init__(self, turns_active: int):
+        """
+        Creates CardState instance.
+        :param turns_active: How many turns will this state last..
+        """
+
+        self.turns_active = turns_active
+        self.active = False
+
+    def card_used(self, effects_to_use: list[Any]):
+        """
+        Card state logic should be put here by overriding this method in concrete card state subclass.
+        :param effects_to_use: List of effects to be used in battle simulation.
+        :return: None.
+        """
+
+        pass
+
+    def update(self):
+        """
+        Updates state, should be called every turn.
+        :return: None
+        """
+
+        self.turns_active -= min(0, self.turns_active - 1)
+        if self.turns_active <= 0:
+            self.active = False
