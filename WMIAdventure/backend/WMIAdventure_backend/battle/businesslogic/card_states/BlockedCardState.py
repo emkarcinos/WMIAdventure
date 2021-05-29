c17e7be4be765334c8d11e2968e49b9fc629039a
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-30 00:37:53 +0200

    Add BlockedCardState class #135

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/card_states/BlockedCardState.py
@@ -0,0 +1,18 @@
+from typing import Any
+
+from battle.businesslogic.card_states.CardState import CardState
+
+
+class BlockedCardState(CardState):
+    """
+    Blocks card from being used by removing it's effects.
+    """
+
+    def card_used(self, effects_to_use: list[Any]):
+        """
+        Blocks card by clearing effects list.
+        :param effects_to_use: List of effects to be used in battle simulation.
+        :return: None.
+        """
+
+        effects_to_use.clear()
