b46a1b61bae320231ab5a041e9ebc2a2ee84dfb9
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-30 17:43:01 +0200

    Add test of method which performs "empty" turns of stopped players #132

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_Coordinator.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/tests_Coordinator.py
@@ -10,9 +10,12 @@ class CoordinatorTestCase(TestCase):
     def setUpClass(cls) -> None:
         cls.creator = Creator()
         cls.profiles = cls.creator.get_user_profile_models()
-        cls.attacker = PlayerFactory.get_instance().create(cls.profiles[0])
-        cls.defender = PlayerFactory.get_instance().create(cls.profiles[1])
-        cls.coordinator = Coordinator(cls.attacker, cls.defender)
+
+    def setUp(self) -> None:
+        self.attacker = PlayerFactory.get_instance().create(self.profiles[0])
+        self.defender = PlayerFactory.get_instance().create(self.profiles[1])
+
+        self.coordinator = Coordinator(self.attacker, self.defender)
 
     def test_next_turn(self):
         self.coordinator.next_turn()
@@ -22,6 +25,19 @@ class CoordinatorTestCase(TestCase):
         defender = self.coordinator.defender
 
         self.assertEqual(attacker, self.coordinator.get_players_opponent(defender))
+
+    def test_get_not_stopped_player(self):
+        """
+        In this test we test whether current turn belongs to defender.
+        Normally it should be attacker's turn, but we stop him.
+        """
+
+        self.attacker.turns_stopped = 3  # Stop attacker
+
+        expected_player = self.defender
+        actual_player = self.coordinator._get_not_stopped_player()  # Get player that is not stopped.
+
+        self.assertIs(actual_player, expected_player)
 
     @classmethod
     def tearDownClass(cls):
