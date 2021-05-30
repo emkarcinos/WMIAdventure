be7c97088457b4eaa8e3e53fac0adedb3804498a
Michael-Czekanski <michal.cz2202@gmail.com>
2021-05-30 02:48:05 +0200

    Add BlockCardEffect #135

---
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/BlockCardEffect.py
@@ -0,0 +1,24 @@
+from battle.businesslogic.card_states.BlockedCardState import BlockedCardState
+from battle.businesslogic.effects.Effect import Effect
+from cards.models import CardLevelEffects
+
+
+class BlockCardEffect(Effect):
+    """
+    Blocks card for given amount of turns.
+    """
+
+    def __init__(self, effect_model: CardLevelEffects, turns_blocked: int=1):
+        super().__init__(effect_model)
+        self.turns_blocked = turns_blocked
+
+    def on_activation(self, target, turns_queue):
+        """
+        Blocks card by adding BlockedCardState to it.
+        :param target: Target player.
+        :param turns_queue:
+        :return: None.
+        """
+
+        card = target.deck.lookup()
+        card.add_state(BlockedCardState(self.turns_blocked))
