f5c9aed992e6bfbc9f5f344ca4e1c2c6057a5123
Marcin Kostrzewski <komar86311@gmail.com>
2021-05-31 22:57:37 +0200

    Added test with buffs and randomization together #247

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/effects/tests_EffectsIntegrations.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/tests/effects/tests_EffectsIntegrations.py
@@ -89,6 +89,37 @@ class EffectsIntegrationsTestCase(TestCase):
         expected_hp_remaining = self.player2.statistics.MAX_HP - dmg_pow
         self.assertEqual(self.player2.get_hp(), expected_hp_remaining)
 
+    def test_buff_next_card_and_randomize(self):
+        buff_card = BattleCard(Card())
+        buff_card.effects.insert(0, self.create_effect(CardEffect.EffectId.EMPOWER,
+                                                       target=CardLevelEffects.Target.OPPONENT,
+                                                       power=20.0))
+
+        randomizing_card = BattleCard(Card())
+        randomizing_card.effects.insert(1, self.create_effect(CardEffect.EffectId.SWAP_RND,
+                                                              target=CardLevelEffects.Target.OPPONENT))
+
+        self.player1.deck.temp_cards_queue.append(buff_card)
+        self.player1.deck.temp_cards_queue.append(randomizing_card)
+        # Buffing next card
+        effects = self.player1.use_card()
+        for e in effects:
+            e.activate(self.player1, self.player2, None)
+
+        # Activating randomization effect
+        effects = self.player1.use_card()
+        # Lets force the randomization until the first card is different than before
+        top_card_before_randomization = self.player2.deck.lookup()
+        while self.player2.deck.lookup() is not top_card_before_randomization:
+            for e in effects:
+                e.activate(self.player1, self.player2, None)
+
+        # Our previous top card is no longer on top, so it should not have any buffs.
+        self.assertEqual(0, len(top_card_before_randomization.effects[0].buffs))
+        # Current top card should have the buff.
+        self.assertEqual(1, len(self.player2.deck.lookup().effects[0].buffs))
+
+
     @classmethod
     def tearDownClass(cls) -> None:
         cls.creator.perform_deletion()
