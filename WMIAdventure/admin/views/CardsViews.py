b7efbac49bfad82460468593e64c48fefd5f32eb
Marcin Kostrzewski <komar86311@gmail.com>
2021-06-13 10:12:00 +0200

    Implemented CardsViews #285

--- WMIAdventure/admin/views/CardsViews.py
+++ WMIAdventure/admin/views/CardsViews.py
@@ -1,3 +1,4 @@
+from models.Card import Card
 from views.View import View
 
 
@@ -5,3 +6,46 @@ class CardsViews(View):
     """
     Class used to translate a Database view into Cards dictionary
     """
+
+    def __init__(self, api_url):
+        """
+        Creates a view into awaiting cards.
+        It also queries them as a cache.
+        If one wishes to refresh the query, call the refresh() method.
+        """
+        super().__init__(api_url)
+
+        # Stores all card objects
+        self.card_objs = []
+
+        self.refresh()
+
+    def _make_cards(self, data: dict) -> list[Card]:
+        """
+        Creates an array of Card object from JSON data.
+        """
+        card_objs = []
+        for card in data:
+            self.card_objs.append(Card(card))
+
+        return card_objs
+
+    def get_all_awaiting_cards(self) -> list[Card]:
+        """
+        Gets all cards from the API.
+        @return An array of Card objects
+        """
+        return self.card_objs
+
+    def get_awaiting_count(self):
+        """
+        Return the count of all awaiting cards.
+        """
+        return len(self.card_objs)
+
+    def refresh(self):
+        """
+        Queries all cards.
+        """
+        cards_data = self._fetch_all()
+        self.card_objs = self._make_cards(cards_data)
--- WMIAdventure/admin/views/View.py
+++ WMIAdventure/admin/views/View.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class View:
     """
     Abstract class to represent a View into a Database and parsing the data to Python dictionaries
@@ -8,3 +11,9 @@ class View:
         Create a view object from a DB query result.
         """
         self.api_url = api_url
+
+    def _fetch_all(self) -> dict:
+        """
+        Fetches data from the API.
+        """
+        return requests.get(self.api_url).json()
