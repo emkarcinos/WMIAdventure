277f46a9401876fafa6ff718bd72e332bc9c1c0d
Marcin Kostrzewski <komar86311@gmail.com>
2021-06-13 17:01:11 +0200

    Added str and repr methods to Cards

--- WMIAdventure/admin/models/Card.py
+++ WMIAdventure/admin/models/Card.py
@@ -6,6 +6,10 @@ class Effect(Model):
     Container for an effect inside a Card
     """
     __slots__ = ['card_effect', 'target', 'power', 'range']
+    
+    def __repr__(self):
+        return f'ID: {self.card_effect}\n' \
+               f'Cel: {self.target}, Moc: {self.power}, Losowość: {self.power}'
 
 
 class Level(Model):
@@ -23,6 +27,11 @@ class Level(Model):
         for effect_dict in dict_effects:
             self.effects.append(Effect(effect_dict))
 
+    def __repr__(self):
+        return f'Poziom: {self.level}  Koszt ulepszenia: {self.next_level_cost}\n' \
+               f'Efekty:\n' \
+               f'\t{self.effects}'
+
 
 class Card(Model):
     """
@@ -38,3 +47,15 @@ class Card(Model):
         self.levels = []
         for level_dict in dict_levels:
             self.levels.append(Level(level_dict))
+
+    def __str__(self):
+        return f'{self.name} (ID: {self.id})'
+
+    def __repr__(self):
+        """
+        Returns a pretty string repr of this card.
+        """
+        return f'Nazwa: {self.name} \t Przedmiot: {self.subject}\n' \
+               f'Opis: {self.tooltip}\n' \
+               f'Poziomy:\n' \
+               f'\t{self.levels}'
