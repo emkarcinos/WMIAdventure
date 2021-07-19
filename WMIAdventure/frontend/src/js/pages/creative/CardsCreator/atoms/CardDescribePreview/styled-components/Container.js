34013f08838a83e94ca744da407e4883e3fe2f82
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-19 20:09:51 +0200

    Add Container styled div component in CardDescribePreview #366

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribePreview/styled-components/Container.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 32px;
+  margin-bottom: 20px;
+  cursor: pointer;
+`;
+
+export default Container;
