6e07c5a8acc46c1a7c3775d57aeb7749119b39a0
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-19 20:14:14 +0200

    Add opening popup when clicking anywhere on CardDescribe section in card editor (Close #366)

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribePreview/CardDescribePreview.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribePreview/CardDescribePreview.js
@@ -2,26 +2,35 @@ import React from 'react';
 import H1 from './styled-components/H1';
 import P from './styled-components/P';
 import Section from './styled-components/Section';
-import Button from './styled-components/Button';
+import Pencil from "./styled-components/Pencil";
+import Container from "./styled-components/Container";
 
 class CardDescribePreview extends React.Component {
     render() {
         return (
             <>
-                <Section>
-                    <H1>
-                        {this.props.cardName}
-                    </H1>
-                    <P>
-                        {this.props.cardSubject}
-                    </P>
-                    <P tooltip>
-                        {this.props.cardTooltip}
-                    </P>
-                    <Button onClick={this.props.showDescribeInputsHandler}>
-                        {/*pensil icon*/}
-                    </Button>
-                </Section>
+                <Container onClick={this.props.showDescribeInputsHandler}>
+                    {/* Pencil icon */}
+                    <Pencil>
+                    </Pencil>
+
+                    {/* Main content section */}
+                    <Section>
+                        <H1>
+                            {this.props.cardName}
+                        </H1>
+                        <P>
+                            {this.props.cardSubject}
+                        </P>
+                        <P tooltip>
+                            {this.props.cardTooltip}
+                        </P>
+                    </Section>
+
+                    {/* Invisible pencil used to center main section in container flexbox */}
+                    <Pencil invisible={true}>
+                    </Pencil>
+                </Container>
             </>
         );
     }
