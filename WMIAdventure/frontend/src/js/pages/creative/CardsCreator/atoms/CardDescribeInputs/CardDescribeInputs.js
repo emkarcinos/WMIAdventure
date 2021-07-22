8229a68f1706de81468dafbe4e584abaa6ae2d75
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-22 11:35:53 +0200

    Add opacity transition when opening card description edition popup (Close #357)

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribeInputs/CardDescribeInputs.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribeInputs/CardDescribeInputs.js
@@ -6,6 +6,14 @@ import Div from './styled-components/Div';
 import Input from './styled-components/Input';
 import Label from './styled-components/Label';
 import DivInput from './styled-components/DivInput';
+import { Transition } from 'react-transition-group';
+
+/* Transition timeout values */
+const timeout = {
+    appear: 50,
+    enter: 50,
+    exit: 300
+};
 
 class CardDescribeInputs extends React.Component {
     state = {
@@ -27,40 +35,44 @@ class CardDescribeInputs extends React.Component {
 
     render() {
         return (
-            <TransparentBack show={this.props.show} onClick={this.handleHiding}>
-                <Fieldset onMouseEnter={this.hoverTrue} onMouseLeave={this.hoverFalse}>
-                    <Legend>
-                        Karta
-                    </Legend>
-                    <Div>
-                        <Label htmlFor='cardName'>
-                            Nazwa
-                        </Label>
-                        <DivInput>
-                            <Input id='cardName' name='cardName' type='text'
-                                   onChange={this.props.updateDescribePreview}/>
-                        </DivInput>
-                    </Div>
-                    <Div>
-                        <Label htmlFor='cardSubject'>
-                            Przedmiot
-                        </Label>
-                        <DivInput>
-                            <Input id='cardSubject' name='cardSubject' type='text'
-                                   onChange={this.props.updateDescribePreview}/>
-                        </DivInput>
-                    </Div>
-                    <Div last>
-                        <Label htmlFor='cardTooltip'>
-                            Opis
-                        </Label>
-                        <DivInput>
-                            <Input id='cardTooltip' name='cardTooltip' type='text'
-                                   onChange={this.props.updateDescribePreview}/>
-                        </DivInput>
-                    </Div>
-                </Fieldset>
-            </TransparentBack>
+            <Transition in={this.props.show} timeout={timeout}>
+                {state => (
+                    <TransparentBack transitionState={state} show={this.props.show} onClick={this.handleHiding}>
+                        <Fieldset onMouseEnter={this.hoverTrue} onMouseLeave={this.hoverFalse}>
+                            <Legend>
+                                Karta
+                            </Legend>
+                            <Div>
+                                <Label htmlFor='cardName'>
+                                    Nazwa
+                                </Label>
+                                <DivInput>
+                                    <Input id='cardName' name='cardName' type='text'
+                                           onChange={this.props.updateDescribePreview}/>
+                                </DivInput>
+                            </Div>
+                            <Div>
+                                <Label htmlFor='cardSubject'>
+                                    Przedmiot
+                                </Label>
+                                <DivInput>
+                                    <Input id='cardSubject' name='cardSubject' type='text'
+                                           onChange={this.props.updateDescribePreview}/>
+                                </DivInput>
+                            </Div>
+                            <Div last>
+                                <Label htmlFor='cardTooltip'>
+                                    Opis
+                                </Label>
+                                <DivInput>
+                                    <Input id='cardTooltip' name='cardTooltip' type='text'
+                                           onChange={this.props.updateDescribePreview}/>
+                                </DivInput>
+                            </Div>
+                        </Fieldset>
+                    </TransparentBack>
+                )}
+            </Transition>
         );
     }
 }
