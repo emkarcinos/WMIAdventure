b203dc89850acb6ce1e3a21f2c6642137b08bfe2
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-22 11:56:53 +0200

    Change styled component Container from div to button #399

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribePreview/styled-components/Container.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribePreview/styled-components/Container.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+const Container = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
   gap: 32px;
   margin-bottom: 20px;
   cursor: pointer;
+  background-color: transparent;
+  border: none;
 `;
 
 export default Container;
