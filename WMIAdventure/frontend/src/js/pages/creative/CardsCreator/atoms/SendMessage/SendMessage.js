62660193dc8fe692f59f3bcf3fd9559c400c8fad
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-23 15:37:59 +0200

    Add shadow animation when SendMessage popup is appearing #357

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendMessage/SendMessage.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendMessage/SendMessage.js
@@ -2,6 +2,14 @@ import React from 'react';
 import TransparentBack from './styled-components/TransparentBack';
 import Div from './styled-components/Div';
 import P from './styled-components/P';
+import { Transition } from 'react-transition-group';
+
+/* Transition timeout values */
+const timeout = {
+    appear: 50,
+    enter: 50,
+    exit: 300
+};
 
 class SendMessage extends React.Component {
 
@@ -31,13 +39,17 @@ class SendMessage extends React.Component {
 
     render() {
         return (
-            <TransparentBack show={this.props.showMessage} onClick={this.handleHiding}>
-                <Div onMouseEnter={this.hoverTrue} onMouseLeave={this.hoverFalse}>
-                    <P success={this.props.sendSuccess}>
-                        {this.state.message}
-                    </P>
-                </Div>
-            </TransparentBack>
+            <Transition in={this.props.showMessage} timeout={timeout}>
+                {state => (
+                    <TransparentBack onClick={this.handleHiding} transitionState={state}>
+                        <Div onMouseEnter={this.hoverTrue} onMouseLeave={this.hoverFalse}>
+                            <P success={this.props.sendSuccess}>
+                                {this.state.message}
+                            </P>
+                        </Div>
+                    </TransparentBack>
+                )}
+            </Transition>
         );
     }
 }
