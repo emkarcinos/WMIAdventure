9ffacd7fb3c11f77a51146b770121db5083cd32f
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-23 16:05:36 +0200

    Add background shadow animation on NavMenu open #357

--- WMIAdventure/frontend/src/js/pages/creative/global/atoms/NavMenu/styled-components/TransparentBack.js
+++ WMIAdventure/frontend/src/js/pages/creative/global/atoms/NavMenu/styled-components/TransparentBack.js
@@ -1,11 +1,17 @@
 import styled from 'styled-components';
 
+const transitionStates = {
+    entering: {opacity: 0, display: 'flex'},
+    entered: {opacity: 1, display: 'flex'},
+    exiting: {opacity: 0, display: 'flex'},
+    exited: {opacity: 0, display: 'none'},
+}
+
 const TransparentBack = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-
-  display: ${({show}) => show ? 'flex' : 'none'};
+  
   flex-direction: column;
   justify-content: start;
   align-items: center;
@@ -14,6 +20,12 @@ const TransparentBack = styled.div`
   width: 100%;
   height: 100%;
   z-index: 3;
+
+  opacity: 0;
+  transition: opacity 0.5s ease;
+  ${
+          ({transitionState}) => transitionStates[transitionState]
+  }
 `;
 
 export default TransparentBack;
