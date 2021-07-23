59549a4e8e321f86243e79b2112f799db1972bcb
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-23 16:29:03 +0200

    Initialize directory and files for SendCardPopup atom #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/SendCardPopup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SendCardPopup extends React.Component {
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default SendCardPopup;
