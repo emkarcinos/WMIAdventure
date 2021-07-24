15d595082c48b3b9c68e488070852fba13346c63
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:39:33 +0200

    Add PopupContainer styled component in SendCardPopup #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/styled-components/PopupContainer.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+const PopupContainer = styled.div`
+  display: flex;
+  
+  /* Flex options*/
+  flex-direction: column;
+  align-items: flex-start;
+  
+  /* Positioning and size */
+  max-width: 95%;
+  position: relative;
+  padding: 24px 24px 64px 24px;
+  @media (max-width: 768px){
+    padding: 16px 16px 32px 16px;
+  }
+  z-index: 3;
+  
+  /* Styling */
+  background: ${({theme}) => theme.colors.ui01};
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+`;
+
+export default PopupContainer;
