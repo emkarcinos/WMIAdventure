4ad23957a048476fc08765f6dfa29b4999e568c1
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:29:47 +0200

    Add TransparentBack styled component #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/styled-components/TransparentBack.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const TransparentBack = styled.div`
+  display: ${({show}) => show ? 'flex' : 'none'};
+  
+  /* Flex options */
+  justify-content: center;
+  align-items: center;
+  
+  /* Positioning and size */
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+
+  /* Styling */
+  background-color: black;
+  opacity: 0.3;
+`;
+
+export default TransparentBack;
