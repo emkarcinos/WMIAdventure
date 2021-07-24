dd5388e409cd4dc76b7d1a8d1a1c3843be6441bc
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:31:21 +0200

    Add TextArea styled component in SendCardPopup #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/styled-components/TextArea.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+const TextArea = styled.textarea`
+  /* Flex options */
+  align-self: stretch;
+  
+  /* textarea options */
+  resize: none;
+  padding: 12px;
+  
+  /* Styling */
+  border: none;
+  background-color: ${({theme}) => theme.colors.grey1};
+  
+  font-size: 18px;
+  font-weight: ${({theme}) => theme.weight.light};
+  
+  :focus {
+    outline: dotted 1px ${({theme}) => theme.colors.grey3};
+  }
+`;
+
+export default TextArea;
