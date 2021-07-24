c0a77ace7620b9786083433421456502db6c5f14
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:41:22 +0200

    Fill SendCardPopup component with appropriate components #371

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/SendCardPopup.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/SendCardPopup.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import CenteringContainer from "./styled-components/CenteringContainer";
+import TransparentBack from "./styled-components/TransparentBack";
+import PopupContainer from "./styled-components/PopupContainer";
+import ContentContainer from "./styled-components/ContentContainer";
+import InfoText from "./styled-components/InfoText";
+import Header from "./styled-components/Header";
+import DivTextArea from "./styled-components/DivTextArea";
+import TextAreaLabel from "./styled-components/TextAreaLabel";
+import TextArea from "./styled-components/TextArea";
+import DivButtons from "./styled-components/DivButtons";
+import Button from "./styled-components/Button";
 
 class SendCardPopup extends React.Component {
     render() {
         return (
             <>
+                <TransparentBack show={this.props.show} onClick={this.props.hideSendCardPopupHandler}>
+                </TransparentBack>
+
+                <CenteringContainer show={this.props.show}>
+                    <PopupContainer>
+                        <ContentContainer>
+                            <Header isMobile={this.props.isMobile}>Wyślij</Header>
+                            <InfoText>
+                                Możesz dołączyć dodatkową wiadomość dla administratorów weryfikujących kartę.
+                            </InfoText>
+                            <DivTextArea>
+                                <TextAreaLabel>Wiadomość</TextAreaLabel>
+                                <TextArea id='additionalComments' name='additionalComments' type='text'
+                                          placeholder={'Wiadomość'}>
+                                </TextArea>
+                            </DivTextArea>
+                            <DivButtons>
+                                <Button>Wróć</Button>
+                                <Button>Wyślij</Button>
+                            </DivButtons>
+                        </ContentContainer>
+                    </PopupContainer>
+                </CenteringContainer>
             </>
+
         );
     }
 }
