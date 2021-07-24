423731a10565826a02e83d885a71f2604b37db17
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 23:07:53 +0200

    Create BeforeSendCard atom and move there some styled components from SendCardPopup molecule #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/BeforeSendCard/BeforeSendCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from "./styled-components/Header";
+import InfoText from "./styled-components/InfoText";
+import DivTextArea from "./styled-components/DivTextArea";
+import TextAreaLabel from "./styled-components/TextAreaLabel";
+import TextArea from "./styled-components/TextArea";
+import DivButtons from "./styled-components/DivButtons";
+import Button from "./styled-components/Button";
+
+class BeforeSendCard extends React.Component {
+    render() {
+        return (
+            <>
+                <Header>Wyślij</Header>
+                <InfoText>
+                    Możesz dołączyć dodatkową wiadomość dla administratorów weryfikujących kartę.
+                </InfoText>
+                <DivTextArea>
+                    <TextAreaLabel htmlFor='additionalComments'>Wiadomość</TextAreaLabel>
+                    <TextArea id='additionalComments' name='additionalComments' type='text'
+                              placeholder={'Wiadomość'}>
+                    </TextArea>
+                </DivTextArea>
+                <DivButtons>
+                    <Button onClick={this.props.hideSendCardPopupHandler}>Wróć</Button>
+                    <Button>Wyślij</Button>
+                </DivButtons>
+            </>
+        )
+    }
+}
+
+export default BeforeSendCard;
--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/molecules/SendCardPopup/SendCardPopup.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/molecules/SendCardPopup/SendCardPopup.js
@@ -3,13 +3,7 @@ import CenteringContainer from "./styled-components/CenteringContainer";
 import TransparentBack from "./styled-components/TransparentBack";
 import PopupContainer from "./styled-components/PopupContainer";
 import ContentContainer from "./styled-components/ContentContainer";
-import InfoText from "./styled-components/InfoText";
-import Header from "./styled-components/Header";
-import DivTextArea from "./styled-components/DivTextArea";
-import TextAreaLabel from "./styled-components/TextAreaLabel";
-import TextArea from "./styled-components/TextArea";
-import DivButtons from "./styled-components/DivButtons";
-import Button from "./styled-components/Button";
+import BeforeSendCard from "../../atoms/BeforeSendCard";
 
 class SendCardPopup extends React.Component {
     render() {
@@ -21,20 +15,7 @@ class SendCardPopup extends React.Component {
                 <CenteringContainer show={this.props.show}>
                     <PopupContainer>
                         <ContentContainer>
-                            <Header>Wyślij</Header>
-                            <InfoText>
-                                Możesz dołączyć dodatkową wiadomość dla administratorów weryfikujących kartę.
-                            </InfoText>
-                            <DivTextArea>
-                                <TextAreaLabel htmlFor='additionalComments'>Wiadomość</TextAreaLabel>
-                                <TextArea id='additionalComments' name='additionalComments' type='text'
-                                          placeholder={'Wiadomość'}>
-                                </TextArea>
-                            </DivTextArea>
-                            <DivButtons>
-                                <Button onClick={this.props.hideSendCardPopupHandler}>Wróć</Button>
-                                <Button>Wyślij</Button>
-                            </DivButtons>
+                            <BeforeSendCard hideSendCardPopupHandler={this.props.hideSendCardPopupHandler} />
                         </ContentContainer>
                     </PopupContainer>
                 </CenteringContainer>
