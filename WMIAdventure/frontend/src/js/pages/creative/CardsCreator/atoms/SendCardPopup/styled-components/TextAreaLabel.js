db3f797d00be8be774d18216a9fe273ec7410921
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:36:45 +0200

    Add TextAreaLabel styled component in SendCardPopup #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/styled-components/TextAreaLabel.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const TextAreaLabel = styled.label`
+  /* Styling */
+  font-weight: ${({theme}) => theme.weight.light};
+  font-size: 14px;
+`;
+
+export default TextAreaLabel;
