f77535e4127be36b8ed7b99c7b82eddb58dcdae3
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:39:49 +0200

    Add CenteringContainer styled component in SendCardPopup #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/styled-components/CenteringContainer.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const CenteringContainer = styled.div`
+  display: ${({show}) => show ? 'flex' : 'none'};
+
+  /* Flex options */
+  justify-content: center;
+  align-items: center;
+
+  /* Positioning and size */
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  /* Styling */
+  background-color: transparent;
+`;
+
+export default CenteringContainer;
