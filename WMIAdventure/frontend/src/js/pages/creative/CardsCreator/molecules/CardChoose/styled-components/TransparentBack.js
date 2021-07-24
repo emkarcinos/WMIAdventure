66a730507747d50c94329b63ddafb5258cc61c06
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 11:33:28 +0200

    Add background shadow animation on CardChoose close #357

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/molecules/CardChoose/styled-components/TransparentBack.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/molecules/CardChoose/styled-components/TransparentBack.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components';
 
+const transitionStates = {
+    entering: {opacity: 0, display: 'flex', 'transition-duration': '0s'},
+    entered: {opacity: 1, display: 'flex', 'transition-duration': '0s'},
+    exiting: {opacity: 0, display: 'flex'},
+    exited: {opacity: 0, display: 'none'},
+}
+
 const TransparentBack = styled.div`
   position: absolute;
   top: 0;
@@ -13,6 +20,13 @@ const TransparentBack = styled.div`
   width: 100%;
   height: 100%;
   z-index: 3;
+
+  /* Transition animation styles */
+  opacity: 0;
+  transition: opacity 0.5s ease;
+  ${
+          ({transitionState}) => transitionStates[transitionState]
+  }
 `;
 
 export default TransparentBack;
