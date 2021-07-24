a28dc3b6282b046337ab4dfa7319213eb3cf087f
Michael-Czekanski <michal.cz2202@gmail.com>
2021-07-24 02:35:09 +0200

    Add DivTextArea styled component in SendCardPopup #371

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/SendCardPopup/styled-components/DivTextArea.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const DivTextArea = styled.div`
+  display: flex;
+  
+  /* Flex options */
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  align-self: stretch;
+  
+`;
+
+export default DivTextArea;
