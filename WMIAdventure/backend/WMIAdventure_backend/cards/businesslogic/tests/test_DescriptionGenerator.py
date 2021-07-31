f80c04e415a0de63587cf09cf9119b754cb65a18
Marcin Kostrzewski <komar86311@gmail.com>
2021-07-31 23:44:58 +0200

    Added generate_description_from_json method test #411

--- WMIAdventure/backend/WMIAdventure_backend/cards/businesslogic/tests/test_DescriptionGenerator.py
+++ WMIAdventure/backend/WMIAdventure_backend/cards/businesslogic/tests/test_DescriptionGenerator.py
@@ -17,3 +17,19 @@ class DescriptionGeneratorTestCase(TestCase):
         generated_description = generator.generate_description([self.effect])
 
         self.assertEqual("Efekt o mocy 3 - 7 skierowany w przeciwnika.", generated_description)
+
+    def test_json(self):
+        generator = DescriptionGenerator.get_instance()
+        data = [{
+            'card_effect': 1,
+            'target': 1,
+            'power': 5,
+            'range': 2,
+        }]
+
+        result = generator.generate_description_from_json(data)
+
+        # I know that this assertion is horrible, but this would turn into a massive integration test if we
+        # were to check for an exact output. That was already tested before and this just really tests whether
+        # the method actually returns anything.
+        self.assertGreater(len(result), 0)
