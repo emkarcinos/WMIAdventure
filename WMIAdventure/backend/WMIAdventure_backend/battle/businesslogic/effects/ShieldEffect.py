405286d375bedea813a8f340773ea1156c37e563
Marcin Kostrzewski <komar86311@gmail.com>
2021-08-01 15:35:43 +0200

    Added descriptions to all effects #411

--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/ShieldEffect.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/ShieldEffect.py
@@ -1,5 +1,6 @@
 from battle.businesslogic.Calculator import Calculator
 from battle.businesslogic.effects.Effect import Effect
+from cards.businesslogic.description_generator.PowerDescription import PowerDescription
 
 
 class ShieldEffect(Effect):
@@ -11,3 +12,7 @@ class ShieldEffect(Effect):
         calculator = Calculator.get_instance()
         shield_amount = calculator.calculate_effect_power(self.power, self.range, self.buffs)
         target.statistics.add_armour(shield_amount)
+
+    def description(self) -> str:
+        power_range = PowerDescription.get_instance().stringify(self.power, self.range)
+        return f"Ochrania {self.target.label}a o {power_range}"
--- WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/SkipCardEffect.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/businesslogic/effects/SkipCardEffect.py
@@ -5,3 +5,6 @@ class SkipCardEffect(Effect):
     def on_activation(self, target, turns_queue):
         # To skip a card, we simply need to dequeue it using Deck's custom queue.
         target.deck.get_card()
+
+    def description(self) -> str:
+        return f"Pomija działanie następnej karty{self.target.label}a"
