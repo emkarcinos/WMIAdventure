0efc6afb6666eb347faa9a5d168a4c56fc4430cc
Michael-Czekanski <michal.cz2202@gmail.com>
2021-08-07 12:07:08 +0200

    Add input for card image #375

---
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/CardDescribeInputs/styled-components/CardImagePreview.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const CardImagePreview = styled.img`
+  /* Size */
+  width: 200px;
+  height: 200px;
+  @media (max-width: 768px){
+    width: 138px;
+    height: 138px;
+  }
+  
+  
+  /* Styling */
+  border-radius: 4px;
+  background-color: ${({theme}) => theme.colors.grey1};
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
+`;
+
+export default CardImagePreview;
