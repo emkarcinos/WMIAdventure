d9186b7ba422c9100723d928250066625afe6f3a
Marcin Kostrzewski <komar86311@gmail.com>
2021-10-07 23:03:21 +0200

    Fixed migration order after merging CardDescription 3

--- WMIAdventure/backend/WMIAdventure_backend/proposed_content/migrations/0005_merge_20211007_2300.py
+++ WMIAdventure/backend/WMIAdventure_backend/proposed_content/migrations/0005_merge_20211007_2300.py
@@ -6,8 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('proposed_content', '0003_proposedcardinfo_comment'),
-        ('proposed_content', '0004_alter_proposedcard_effects_description'),
+        ('proposed_content', '0004_proposedcardinfo_comment'),
+        ('proposed_content', '0005_alter_proposedcard_effects_description'),
     ]
 
     operations = [
