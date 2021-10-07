c9d044e4ae288498d31bb9317b2fd94199c93461
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-07 16:39:08 +0200

    Fix effect search causing effects not being displayed

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/Effect/styled-components/Button.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/atoms/Effect/styled-components/Button.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 function visibilityHandler(name, searchInput) {
-  if (name.includes(searchInput) || searchInput === '') {
+  if (name.includes(searchInput) || searchInput === '' || typeof searchInput === 'undefined') {
     return 'flex';
   }
   return 'none';
