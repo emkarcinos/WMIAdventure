28e531113943f8b70eaad6f0cd1de5b21ebff020
Marcin Kostrzewski <komar86311@gmail.com>
2021-10-11 18:30:39 +0200

    #488 Tweaked card list sizing & hidden scrollbar

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreator/molecules/CardChoose/styled-components/Ul.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreator/molecules/CardChoose/styled-components/Ul.js
@@ -5,13 +5,24 @@ const Ul = styled.ul`
   display: flex;
   flex-direction: column;
 
-  width: 80%;
-  height: 80%;
+  @media(max-width: ${({theme}) => theme.overMobile}px) {
+    width: 92%;
+  }
+  @media(min-width: ${({theme}) => theme.overMobile}px) {
+    width: 440px
+  }
+  
+  height: 75%;
 
   padding: 0 10px;
   margin: 0;
   border-radius: 20px;
   overflow-y: scroll;
+  scrollbar-width: none;
+  -ms-overflow-style: none; /* IE 10+ */
+  ::-webkit-scrollbar {
+    display: none; /* Chrome Safari */
+  }
 `;
 
 export default Ul;
