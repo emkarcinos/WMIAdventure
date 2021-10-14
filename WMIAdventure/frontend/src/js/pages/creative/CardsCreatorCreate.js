28e22b444311f03121146976067025bf47fa038f
Dawid Korybalski <dawid.korybalski@gmail.com>
2021-10-14 21:36:27 +0200

    Added title section

--- WMIAdventure/frontend/src/js/pages/creative/CardsCreatorCreate.js
+++ WMIAdventure/frontend/src/js/pages/creative/CardsCreatorCreate.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import CardsCreator from './CardsCreator';
 
 class CardsCreatorCreate extends React.Component {
     render() {
-        return (
-            <CardsCreator creatorType='create' />
+        return (                
+            <>
+                <Helmet>
+                    <title>Kreator kart WMI Adventure</title>
+                </Helmet>
+                <CardsCreator creatorType='create' />
+            </>
         );
     }
 }
--- WMIAdventure/frontend/src/js/pages/entertainment/Profile/Profile.js
+++ WMIAdventure/frontend/src/js/pages/entertainment/Profile/Profile.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import {Helmet} from 'react-helmet';
 import NavBar from '../MainMenu/organisms/NavBar';
 import StyledWrapper from './StyledWrapper';
 
 function Profile() {
     return (
-        <StyledWrapper>
-            <NavBar />
-            <p>
-                Profile view - not implemented yet.
-            </p>
-        </StyledWrapper>
+        <>
+            <Helmet>
+                <title>Profil użytkownika</title>
+            </Helmet>
+            <StyledWrapper>
+                <NavBar />
+                <p>
+                    Profile view - not implemented yet.
+                </p>
+            </StyledWrapper>
+        </>
     );
 }
 
