df12fc352d8f7aba7c54e6146238088b0eb6b6f4
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-15 22:47:30 +0200

    Add PageButton styled component in Pager atom component #536

---
+++ WMIAdventure/frontend/src/js/pages/entertainment/BattleMode/atoms/Pager/styled-components/PageButton.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const PageButton = styled.button`
+  /* Flex options */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  /* Sizing */
+  width: 32px;
+  height: 32px;
+  
+  /* Styling */
+  background: ${({theme, disabled}) => !disabled ? theme.colors.grey1 : theme.colors.grey2};
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  border-style: none;
+
+  /* Font */
+  color: ${({theme, disabled}) => !disabled ? theme.colors.text01 : theme.colors.grey1};
+  font-family: 'Open Sans', sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  text-align: center;
+`;
+
+export default PageButton;
