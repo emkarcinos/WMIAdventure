42e09e73ebe4c39527884b313c3f27afda9013e2
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-16 21:00:04 +0200

    Add sending post requests in RequestSender class #516

--- WMIAdventure/frontend/src/js/api/RequestSender.js
+++ WMIAdventure/frontend/src/js/api/RequestSender.js
@@ -16,6 +16,23 @@ class RequestSender {
             response => response.json()
         );
     }
+
+    /**
+     * Sends POST request.
+     * @param url
+     * @param body Request body. You have to prepare body in the format that you want it to be. (json, form data, etc.)
+     * @param headers
+     * @returns {Promise<any>} Response.
+     */
+    static post = (url, body, headers={}) => {
+        return fetch(url, {
+            method: 'post',
+            headers: headers,
+            body: body
+        }).then(
+            response => response
+        );
+    }
 }
 
 export default RequestSender;
