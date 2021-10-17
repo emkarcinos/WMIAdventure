85aa9399b354ae75a4b07d0b1c8983df47cbe03f
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-17 13:10:32 +0200

    Comment func #516

--- WMIAdventure/frontend/src/js/api/gateways/BattleAPIGateway.js
+++ WMIAdventure/frontend/src/js/api/gateways/BattleAPIGateway.js
@@ -1,6 +1,11 @@
 import RequestSender from "../RequestSender";
 import BattleEndpoints from "../endpoints/BattleEndpoints";
 
+/**
+ * Starts battle between logged in user and given opponent.
+ * @param userId Opponent.
+ * @returns {Promise<*>} Battle result.
+ */
 const fightWithUser = (userId) => {
     return RequestSender.get(BattleEndpoints.fightWithUser(userId));
 }
