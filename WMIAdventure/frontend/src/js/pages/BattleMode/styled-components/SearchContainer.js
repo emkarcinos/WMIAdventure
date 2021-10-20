be95786076e1cedb2155214d31bafa8ec07a1240
Mateusz <wirus006@gmail.com>
2021-10-20 22:20:18 +0200

    correct SearchContainer pos #526

--- WMIAdventure/frontend/src/js/pages/BattleMode/styled-components/SearchContainer.js
+++ WMIAdventure/frontend/src/js/pages/BattleMode/styled-components/SearchContainer.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 const SearchContainer = styled.div`
+  width: 100%;
+  max-width: 340px;
+  margin: 0 auto;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
   background-color: ${({theme}) => theme.colors.ui01};
