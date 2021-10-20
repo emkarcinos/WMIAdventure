7e635e3b17aa464921b3ca4b329974cac2848d58
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-20 21:39:52 +0200

    Change props login to displayedUsername #526

--- WMIAdventure/frontend/src/js/components/battle/atoms/UserListItem/UserListItem.js
+++ WMIAdventure/frontend/src/js/components/battle/atoms/UserListItem/UserListItem.js
@@ -12,13 +12,13 @@ import battleIcon from './../../../../../assets/images/battleIcon.png';
 class UserListItem extends React.Component {
     render() {
         return (
-            <Item login={this.props.login} searchInput={this.props.searchInput}>
+            <Item displayedUsername={this.props.displayedUsername} searchInput={this.props.searchInput}>
                 <AvatarContainer access={this.props.access}>
                     {this.props.avatar ? <Avatar access={this.props.access} src={this.props.avatar} alt=""/> : ''}
                 </AvatarContainer>
                 <Div>
                     <Login access={this.props.access}>
-                        {this.props.login}
+                        {this.props.displayedUsername}
                     </Login>
                     <LabelsContainer>
                         <Label term={this.props.term}>
