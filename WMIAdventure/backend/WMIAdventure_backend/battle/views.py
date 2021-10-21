43ca1a61bfbcbd241e47105a0d221c97e9f2f253
Marcin Kostrzewski <komar86311@gmail.com>
2021-10-21 20:12:36 +0200

    #521 BattleView is now authenticated

--- WMIAdventure/backend/WMIAdventure_backend/battle/views.py
+++ WMIAdventure/backend/WMIAdventure_backend/battle/views.py
@@ -1,3 +1,5 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -9,6 +11,9 @@ from users.models import User
 
 
 class BattleView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     """
     **get**:
 
