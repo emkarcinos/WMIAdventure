1c917f936ea0784b318a72f6ce740a28450b629a
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-23 14:40:35 +0200

    Add styled components for LoginPage #523

---
+++ WMIAdventure/frontend/src/js/pages/LoginPage/styled-components/WholePageDiv.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const WholePageDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  
+  align-items: center;
+  gap: 32px;
+`
+
+export default WholePageDiv;
