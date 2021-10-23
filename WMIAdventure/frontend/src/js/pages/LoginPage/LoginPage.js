62483dea31dacf0c90edbd18d90dca97a4e3efc3
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-23 20:15:27 +0200

    Code refactor, add redirecting to main page #523

--- WMIAdventure/frontend/src/js/pages/LoginPage/LoginPage.js
+++ WMIAdventure/frontend/src/js/pages/LoginPage/LoginPage.js
@@ -1,49 +1,12 @@
 import React from 'react';
 import {Helmet} from 'react-helmet';
 import WholePageDiv from "./styled-components/WholePageDiv";
-import UsersAPIGateway from "../../api/gateways/UsersAPIGateway";
-import LoginForm from "../../components/auth/molecules/LoginForm";
 import StyledWrapper from "../MainMenu/StyledWrapper";
 import NavBar from "../../components/prototype/organisms/NavBar";
+import Login from "../../components/auth/organisms/Login";
 
 
 class LoginPage extends React.Component {
-    state = {
-        username: null
-    }
-
-    loginFailedHandler = (response) => {
-        let msg;
-
-        if (response.status === 404) {
-            msg = 'User not found.';
-        }
-
-        alert(`Login failed. ${msg} Status: ${response.status}`);
-    }
-
-    loginSuccessHandler = () => {
-        alert(`Logged in as ${this.state.username}`);
-    }
-
-    submitHandler = (event) => {
-        event.preventDefault();
-
-        UsersAPIGateway.login(this.state.username)
-            .then(response => {
-                if (response.ok)
-                    this.loginSuccessHandler();
-                else
-                    this.loginFailedHandler(response);
-            })
-            .catch(err => {console.log(err); this.loginFailedHandler()});
-    }
-
-    usernameChangedHandler = (event) => {
-        event.preventDefault();
-        this.setState({username: event.target.value})
-    }
-
     render() {
         return (
             <>
@@ -54,12 +17,8 @@ class LoginPage extends React.Component {
                 <StyledWrapper>
                     <NavBar/>
                     <WholePageDiv>
-                        <h1>Login Page</h1>
-
-                        <LoginForm
-                            usernameChangedHandler={this.usernameChangedHandler}
-                            submitHandler={this.submitHandler}
-                        />
+                        <h1>Login</h1>
+                        <Login />
                     </WholePageDiv>
                 </StyledWrapper>
             </>
