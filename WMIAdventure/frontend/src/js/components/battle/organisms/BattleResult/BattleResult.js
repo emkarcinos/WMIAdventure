0d5b807d20081d115ba7fc923afb3f4bb0d32c39
Mateusz <wirus006@gmail.com>
2021-10-23 10:02:52 +0200

    initialize BattleResult component

---
+++ WMIAdventure/frontend/src/js/components/battle/organisms/BattleResult/BattleResult.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import MobilePopUp from '../MobilePopUp';
+import UserInfo from '../../atoms/UserInfo';
+
+class BattleResult extends React.Component {
+    render() {
+        return (
+            <MobilePopUp>
+                <article>
+                    <h2>
+
+                    </h2>
+                    <img />
+                    <UserInfo />
+
+                </article>
+            </MobilePopUp>
+        );
+    }
+}
+
+export default BattleResult;
