938efcb03755eac28363ee21d6a3844d3fb0a405
Michael-Czekanski <michal.cz2202@gmail.com>
2021-10-23 22:11:45 +0200

    Add funcs checking if logged in, logging out #525

--- WMIAdventure/frontend/src/js/api/Cookies.js
+++ WMIAdventure/frontend/src/js/api/Cookies.js
@@ -3,8 +3,11 @@ import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
 const getSessionToken = () => {
-    console.log(`cookies['session']: ${cookies.get('session')}`)
     return cookies.get('session');
 }
 
-export default {getSessionToken};
+const removeSessionToken = () => {
+    cookies.remove('session');
+}
+
+export default {getSessionToken, removeSessionToken};
