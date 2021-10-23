5e8449c4a8e59aaaf9cdf161e3c1294aee6290f0
Mateusz <wirus006@gmail.com>
2021-10-23 11:33:20 +0200

    change aligment from (center,end) to space-around in SwipeProfile #558

--- WMIAdventure/frontend/src/js/components/battle/organisms/SwipeProfile/styled-components/Article.js
+++ WMIAdventure/frontend/src/js/components/battle/organisms/SwipeProfile/styled-components/Article.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 const Article = styled.article`
-  display: ${({visible}) => visible ? 'grid' : 'none'};
-  grid-template-rows: 1fr auto;
+  display: ${({visible}) => visible ? 'flex' : 'none'};
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
   width: 100%;
   min-height: 100%;
   padding: 36px 0 16px 0;
--- WMIAdventure/frontend/src/js/components/battle/organisms/SwipeProfile/styled-components/FlexCenterContainer.js
+++ WMIAdventure/frontend/src/js/components/battle/organisms/SwipeProfile/styled-components/FlexCenterContainer.js
@@ -5,8 +5,6 @@ const FlexCenterContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100%;
-  min-height: 100%;
 `;
 
 export default FlexCenterContainer;
