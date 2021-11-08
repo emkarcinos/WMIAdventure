8aa7d95ed7a330f72d53f4ef2a21618c126c94eb
Marcin Kostrzewski <komar86311@gmail.com>
2021-11-08 16:06:35 +0100

    Update WMIAdventure/frontend/src/js/components/battle/molecules/EnemyStateContainer/styled-components/Nick.js

--- WMIAdventure/frontend/src/js/components/battle/molecules/EnemyStateContainer/styled-components/Nick.js
+++ WMIAdventure/frontend/src/js/components/battle/molecules/EnemyStateContainer/styled-components/Nick.js
@@ -4,7 +4,7 @@ const Nick = styled.p`
   margin: 0 0 0 10px;
   font-family: 'Open Sans', sans-serif;
   font-size: 16px;
-  font-weight: ${({theme}) => theme.weight.medium};
+  font-weight: ${({theme}) => theme.weight.regular};
   color: ${({theme}) => theme.colors.light2};
 `;
 
