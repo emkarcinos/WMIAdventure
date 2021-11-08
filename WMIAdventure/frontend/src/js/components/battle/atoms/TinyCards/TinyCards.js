8f8e05cddf0a28d3c9af3f0b0f1725b237f4e23a
Marcin Kostrzewski <komar86311@gmail.com>
2021-11-08 17:11:29 +0100

    #621 Card without image is different from hidden card

--- WMIAdventure/frontend/src/js/components/battle/atoms/TinyCards/TinyCards.js
+++ WMIAdventure/frontend/src/js/components/battle/atoms/TinyCards/TinyCards.js
@@ -9,18 +9,18 @@ class TinyCards extends Deck {
     constructor(props) {
         super(props);
         this.state = {
-            card1 : null,
-            card2 : null,
-            card3 : null,
-            card4 : null,
-            card5 : null,
+            card1: null,
+            card2: null,
+            card3: null,
+            card4: null,
+            card5: null,
         };
     }
 
     getImages = (i) => {
         let contents = [];
         for (const [, card] of Object.entries(this.state)) {
-            card ? contents.push(card.image) : contents.push(null);
+            card ? contents.push(card.image) : contents.push(unknownIcon);
         }
         return contents[i];
     }
@@ -29,10 +29,10 @@ class TinyCards extends Deck {
         return (
             <MainContainer setMargin={this.props.setMargin} gap={this.props.gap}>
                 {[...Array(5)].map(
-                    (e,i) => {
+                    (e, i) => {
                         return (
                             <ImageContainer key={`cardImage-${i}`}>
-                                <CardImage src={this.getImages(i) ? this.getImages(i) : unknownIcon} />
+                                <CardImage src={this.getImages(i) ? this.getImages(i) : unknownIcon}/>
                             </ImageContainer>
                         );
                     }
