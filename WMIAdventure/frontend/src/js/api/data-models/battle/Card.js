3ddd767aedf2589002d682a4e6d9deb72d5df820
Marcin Kostrzewski <komar86311@gmail.com>
2021-11-19 17:32:24 +0100

    #600 Added extra fields for Card

--- WMIAdventure/frontend/src/js/api/data-models/battle/Card.js
+++ WMIAdventure/frontend/src/js/api/data-models/battle/Card.js
@@ -1,6 +1,11 @@
 export class Card {
     id = undefined;
     level = undefined;
+    image = null;
+    name = '';
+    subject = '';
+    tooltip = '';
+    description = '';
     buffs = [];
     initialPosition = 0;
     stoppedTurns = 0;
@@ -14,6 +19,16 @@ export class Card {
         if (this.stoppedTurns > 0)
             this.stoppedTurns--;
         this.buffs = [];
+    }
+
+    async fetchFieldsFromBackend(fetchFunction) {
+        const data = await fetchFunction(this.id);
+        const description = data.levels.filter(level => level.level === this.level)[0].effects_description;
+        this.name = data.name;
+        this.image = data.image;
+        this.subject = data.subject;
+        this.tooltip = data.tooltip;
+        this.description = description;
     }
 
 }
--- WMIAdventure/frontend/src/js/api/data-models/battle/test/Card.test.js
+++ WMIAdventure/frontend/src/js/api/data-models/battle/test/Card.test.js
@@ -13,3 +13,32 @@ test('Should not decrement stoppedTurns below zero', () => {
     card.onTurnEnd();
     expect(card.stoppedTurns).toBe(0);
 })
+
+test('Should fetch from backend correctly', async () => {
+    const card = new Card(15, 2);
+    const mockBackendResponse = {
+        id: card.id,
+        name: "nm",
+        subject: "sub",
+        image: "img",
+        tooltip: "fof",
+        levels: [
+            {
+                level: 1,
+                effects_description: "level1desc",
+            },
+            {
+                level: 2,
+                effects_description: "level2desc",
+            },
+        ]
+    }
+    const mockCallback = jest.fn(() => mockBackendResponse);
+    await card.fetchFieldsFromBackend(mockCallback);
+    expect(card.subject).toBe(mockBackendResponse.subject);
+    expect(card.name).toBe(mockBackendResponse.name);
+    expect(card.image).toBe(mockBackendResponse.image);
+    expect(card.tooltip).toBe(mockBackendResponse.tooltip);
+    expect(card.description).toBe(mockBackendResponse.levels[1].effects_description);
+
+})
