5350c3efa92e1b6608601a11802d90ce7b20eafc
Marcin Kostrzewski <komar86311@gmail.com>
2021-11-20 21:37:27 +0100

    #600 Fixed tests for new battle behaviour

--- WMIAdventure/frontend/src/js/api/data-models/battle/test/Battle.test.js
+++ WMIAdventure/frontend/src/js/api/data-models/battle/test/Battle.test.js
@@ -7,7 +7,7 @@ const exampleData1 = {
             hp: 100.0,
             armour: 6.0
         },
-        deck: [],
+        deck: [{id: 1, level: 1}],
     },
     defender: {
         id: 152,
@@ -15,11 +15,63 @@ const exampleData1 = {
             hp: 25.2,
             armour: 2.2
         },
-        deck: [],
+        deck: [{id: 1, level: 1}],
     },
     outcome: {
         winner: 10
     }
+}
+
+const exampleDataWithTurns = {
+    ...exampleData1,
+    turns: [
+        {
+            attacker: {
+                stats: exampleData1.attacker.stats,
+                deck: [1]
+            },
+            defender: {
+                stats: exampleData1.defender.stats,
+                deck: [1]
+            },
+            card_executor: exampleData1.attacker.id,
+            used_card: 3,
+            used_effects: [
+                {
+                    id: 1,
+                    target_player: exampleData1.defender.id,
+                    power: 37.87219609052145,
+                    changed_stats: {
+                        hp: 62.12780390947855,
+                        armour: 0.0
+                    }
+                },
+            ],
+        },
+        {
+            attacker: {
+                stats: exampleData1.attacker.stats,
+                deck: [1]
+            },
+            defender: {
+                stats: exampleData1.defender.stats,
+                deck: [1]
+            },
+            card_executor: exampleData1.defender.id,
+            used_card: 3,
+            used_effects: [
+                {
+                    id: 1,
+                    target_player: exampleData1.attacker.id,
+                    power: 37.87219609052145,
+                    changed_stats: {
+                        hp: 62.12780390947855,
+                        armour: 0.0
+                    }
+                },
+            ],
+        },
+    ]
 }
 
 test("Create Battle", () => {
@@ -29,37 +81,17 @@ test("Create Battle", () => {
 })
 
 test("Test nextTurn returns correct turns", () => {
-    const turns = [{card_executor: 1}, {card_executor: 2}];
-    let data = exampleData1;
-    data.turns = turns;
-    const battle = battleFromData(data);
+    const battle = battleFromData(exampleDataWithTurns);
     const turn1 = battle.nextTurn();
     const turn2 = battle.nextTurn();
     const turn3 = battle.nextTurn();
-    expect(turn1.executorId).toBe(turns[0].card_executor);
-    expect(turn2.executorId).toBe(turns[1].card_executor);
+    expect(turn1.executorId).toBe(exampleDataWithTurns.turns[0].card_executor);
+    expect(turn2.executorId).toBe(exampleDataWithTurns.turns[1].card_executor);
     expect(turn3).toBe(null);
 })
 
 test("Should isUsersTurn get assigned correctly", () => {
-    const exampleTurnData = [{
-        card_executor: exampleData1.attacker.id,
-        used_card: 3,
-        used_effects: [
-            {
-                id: 1,
-                target_player: exampleData1.defender.id,
-                power: 37.87219609052145,
-                changed_stats: {
-                    hp: 62.12780390947855,
-                    armour: 0.0
-                }
-            },
-        ],
-    }]
-    let data = exampleData1;
-    data.turns = exampleTurnData;
-    const battle = battleFromData(data);
+    const battle = battleFromData(exampleDataWithTurns);
     battle.nextTurn();
     expect(battle.isUsersTurn).toBeTruthy();
 })
