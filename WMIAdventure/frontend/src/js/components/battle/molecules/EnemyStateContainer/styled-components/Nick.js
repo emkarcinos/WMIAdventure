84890971f7dd829e589316a3840028cdb5ec4ecd
Marcin Kostrzewski <komar86311@gmail.com>
2021-11-21 14:50:15 +0100

    #600 Username style fixes to match Figma

--- WMIAdventure/frontend/src/js/components/battle/molecules/EnemyStateContainer/styled-components/Nick.js
+++ WMIAdventure/frontend/src/js/components/battle/molecules/EnemyStateContainer/styled-components/Nick.js
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const Nick = styled.p`
   margin: 0 0 0 10px;
   font-family: 'Open Sans', sans-serif;
-  font-size: 16px;
-  font-weight: ${({theme}) => theme.weight.regular};
+  font-size: 18px;
+  font-weight: ${({theme}) => theme.weight.semibold};
   color: ${({theme}) => theme.colors.light2};
 
   @media (min-width: ${({theme}) => theme.overMobile}px) {
