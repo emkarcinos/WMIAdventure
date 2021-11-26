c628b821ab35cc3bc796570ad15ed32f976dddb4
Marcin Kostrzewski <komar86311@gmail.com>
2021-11-26 16:37:27 +0100

    #600 getCurrentFullCard cleanup

--- WMIAdventure/frontend/src/js/components/battle/organisms/BattleView/BattleView.js
+++ WMIAdventure/frontend/src/js/components/battle/organisms/BattleView/BattleView.js
@@ -501,33 +501,20 @@ class BattleView extends React.Component {
 
     getCurrentFullCard = () => {
         const card = this.state.battle.getCardOnTop();
-
-        // I had to do it this way because of the way props are in FullCardView
-        if (card.level === 1) {
-            return (
-                <FullCardView cardName={card.name} cardSubject={card.subject}
-                              cardImage={card.image} cardTooltip={card.tooltip}
-                              description={card.description} common
-                              setTranslateY={this.state.fullCardAction.translateY}/>
-            )
-        } else if (card.level === 2) {
-            return (
-                <FullCardView cardName={card.name} cardSubject={card.subject}
-                              cardImage={card.image} cardTooltip={card.tooltip}
-                              description={card.description} gold
-                              setTranslateY={this.state.fullCardAction.translateY}/>
-            )
-        } else {
-            return (
-                <FullCardView cardName={card.name} cardSubject={card.subject}
-                              cardImage={card.image} cardTooltip={card.tooltip}
-                              description={card.description} epic
-                              setTranslateY={this.state.fullCardAction.translateY}/>
-            )
+        const levels = {
+            common: card.level === 1,
+            gold: card.level === 2,
+            epic: card.level === 3,
         }
-
+        return (
+            <FullCardView cardName={card.name} cardSubject={card.subject}
+                          cardImage={card.image} cardTooltip={card.tooltip}
+                          description={card.description} common={levels.common}
+                          gold={levels.gold} epic={levels.epic}
+                          setTranslateY={this.state.fullCardAction.translateY}/>
+        )
     }
-
+    
     render() {
         if (this.state.battle === undefined) return (<></>);
         return (
