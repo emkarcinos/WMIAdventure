14b83f8765c96ed433ce377031d8af90ba37a766
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-01 18:09:21 +0100

    #341 Added new navbar

---
+++ WMIAdventure/frontend/src/js/components/global/molecules/Navbar/styled-components/Nav.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  height: 56px;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 28px;
+`;
+
+export default Nav;
--- WMIAdventure/frontend/src/js/pages/MainMenu/MainMenu.js
+++ WMIAdventure/frontend/src/js/pages/MainMenu/MainMenu.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-import NavBar from '../../components/prototype/organisms/NavBar';
+import {Helmet} from 'react-helmet';
 import GridOneColumn from '../../components/prototype/molecules/GridOneColumn';
 import MainMenuModule from '../../components/prototype/atoms/MainMenuModule';
 import adventureImg from '../../../assets/images/adventure-image.png';
 import battleImg from '../../../assets/images/battle-image.png';
 import rankingImg from '../../../assets/images/ranking-image.png';
 import StyledWrapper from './StyledWrapper';
+import Navbar from "../../components/global/molecules/Navbar";
 
 
 function MainMenu() {
@@ -16,14 +16,18 @@ function MainMenu() {
                 <title>Strona główna WMI Adventure</title>
             </Helmet>
             <StyledWrapper className="MainMenu">
-                <NavBar />
+                <Navbar/>
                 <GridOneColumn rowGaps='16px' margin='80px 0 16px 0'>
-                    <MainMenuModule img={adventureImg} alt={'Zdjęcie wejścia do wydziału WMI.'} link={'/adventure'} header={'ADVENTURE'} describe={'Przeżyj wydziałową historię w postaci Visual Novel.'} />
-                    <MainMenuModule img={battleImg} alt={'Orkowie.'} header={'BATTLE'} link={'/battle'} describe={'Walcz z innymi graczami.'} />
-                    <MainMenuModule img={rankingImg} alt={'Magowie.'} header={'RANKINGI'} link={'/ranking'} describe={'Sprawdź jakim prosem jesteś.'} />
+                    <MainMenuModule img={adventureImg} alt={'Zdjęcie wejścia do wydziału WMI.'} link={'/adventure'}
+                                    header={'ADVENTURE'}
+                                    describe={'Przeżyj wydziałową historię w postaci Visual Novel.'}/>
+                    <MainMenuModule img={battleImg} alt={'Orkowie.'} header={'BATTLE'} link={'/battle'}
+                                    describe={'Walcz z innymi graczami.'}/>
+                    <MainMenuModule img={rankingImg} alt={'Magowie.'} header={'RANKINGI'} link={'/ranking'}
+                                    describe={'Sprawdź jakim prosem jesteś.'}/>
                 </GridOneColumn>
             </StyledWrapper>
-            </>
+        </>
     );
 }
 
