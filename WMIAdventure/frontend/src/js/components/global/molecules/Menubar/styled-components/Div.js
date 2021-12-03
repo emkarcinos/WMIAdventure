c0e9e27ae8ec441851d39a88549a3ca156dce2a1
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-03 22:53:13 +0100

    #704 Added desktop variant for Navmenu

--- WMIAdventure/frontend/src/js/components/global/molecules/Menubar/styled-components/Div.js
+++ WMIAdventure/frontend/src/js/components/global/molecules/Menubar/styled-components/Div.js
@@ -4,12 +4,18 @@ const Div = styled.div`
   position: fixed;
   bottom: 0;
   right: 0;
+
   width: 254px;
+  transform: ${({visible}) => visible ? 'translateX(0)' : 'translateX(254px)'};
   height: calc(100vh - calc(100vh - 100%));
   background-color: ${({theme}) => theme.colors.whiteAlmost};
-  transform: ${({visible}) => visible ? 'translateX(0)' : 'translateX(254px)'};
   transition: transform 0.2s ease-in-out;
   z-index: 20;
+
+  @media (min-width: ${({theme}) => theme.overMobile}px) {
+    width: 350px;
+    transform: ${({visible}) => visible ? 'translateX(0)' : 'translateX(350px)'};
+  }
 `;
 
 export default Div;
