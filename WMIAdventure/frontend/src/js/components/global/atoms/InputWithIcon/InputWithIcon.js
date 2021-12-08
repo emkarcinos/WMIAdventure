c922307c61ea367b075db2ddb4bd3df6510b308e
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-08 21:07:35 +0100

    #747 Added InputWithIcon component

---
+++ WMIAdventure/frontend/src/js/components/global/atoms/InputWithIcon/InputWithIcon.js
@@ -0,0 +1,19 @@
+import React from "react";
+import FlexGapContainer from "../../molecules/FlexGapContainer/FlexGapContainer";
+import Input from "./styled-components/Input";
+import Image from "./styled-components/Image";
+
+class InputWithIcon extends React.Component {
+
+    render() {
+        return (
+            <FlexGapContainer gap={'2px'}>
+                <Input width={this.props.width} type={this.props.type} min={this.props.min} maxLength={this.props.max}/>
+                <Image src={this.props.icon} alt={'Edit icon'}/>
+            < /FlexGapContainer>
+        )
+
+    }
+}
+
+export default InputWithIcon;
---
+++ WMIAdventure/frontend/src/js/components/global/atoms/InputWithIcon/styled-components/Image.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 10px;
+  height: 10px;
+`;
+
+export default Image;
