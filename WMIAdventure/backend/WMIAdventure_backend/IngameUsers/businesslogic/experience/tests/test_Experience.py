1a60bb196e1039694329a6fb6f37c90f4e2bdd53
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-15 21:52:35 +0100

    #796 Added percentage calculation in Experience

--- WMIAdventure/backend/WMIAdventure_backend/IngameUsers/businesslogic/experience/tests/test_Experience.py
+++ WMIAdventure/backend/WMIAdventure_backend/IngameUsers/businesslogic/experience/tests/test_Experience.py
@@ -30,3 +30,22 @@ class ExperienceTestCase(unittest.TestCase):
         experience_obj = Experience(exp)
         expected_exp_required = abs(exp - level_to_exp(level + 1))
         self.assertEqual(experience_obj.exp_required_to_level_up, expected_exp_required)
+
+    def test_should_calculate_percentage_correctly(self):
+        exp = 5
+        # Next level is at 10
+        experience_obj = Experience(exp)
+        self.assertEqual(experience_obj.next_level_percent, 50)
+
+    def test_should_calculate_percentage_correctly_when_zero_exp(self):
+        exp = 0
+        # Next level is at 10
+        experience_obj = Experience(exp)
+        self.assertEqual(experience_obj.next_level_percent, 0)
+
+    def test_should_calculate_percentage_correctly_when_100_percent(self):
+        exp = 10
+        # Next level is at 10
+        experience_obj = Experience(exp)
+        # Should not return 100, because we are a level higher
+        self.assertEqual(experience_obj.next_level_percent, 0)
