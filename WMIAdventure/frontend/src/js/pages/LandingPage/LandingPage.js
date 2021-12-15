ea059af16333f099b77d6b681c19135edb40f3f7
Mateusz <wirus006@gmail.com>
2021-12-15 19:41:36 +0100

    add landing page to router #694

---
+++ WMIAdventure/frontend/src/js/pages/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+    render() {
+        return (
+            <div>
+                landing page
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
