0db991cdff3993afc00fd6a06c607034286a69ba
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-16 22:38:15 +0100

    #700 Fix tests

--- WMIAdventure/backend/WMIAdventure_backend/users/models.py
+++ WMIAdventure/backend/WMIAdventure_backend/users/models.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
     """Custom User class."""
 
-    email = None
+    email = models.EmailField(blank=True, unique=False, max_length=254, verbose_name='email address')
     first_name = None
     last_name = None
