8bab583cada089781311e5d3c6939c1c45426161
Mateusz <wirus006@gmail.com>
2021-12-16 12:27:00 +0100

    increase line-height article paragraph #694

--- WMIAdventure/frontend/src/js/components/landing-page/molecules/LandingArticle/styled-components/Paragraph.js
+++ WMIAdventure/frontend/src/js/components/landing-page/molecules/LandingArticle/styled-components/Paragraph.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Paragraph = styled.p`
   font-family: 'Open Sans', sans-serif;
   font-size: 14px;
-  line-height: 20px;
+  line-height: 22px;
   font-weight: ${({theme}) => theme.weight.regular};
   color: ${({theme}) => theme.colors.dark};
 `;
