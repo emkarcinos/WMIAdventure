09489f800fa2766c282350aa9b76bfa31aca0770
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-16 22:37:00 +0100

    #700 Remove unsafe auth token view

--- WMIAdventure/backend/WMIAdventure_backend/users/views.py
+++ WMIAdventure/backend/WMIAdventure_backend/users/views.py
@@ -1,6 +1,5 @@
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -35,25 +34,6 @@ class UserList(APIView):
 
         serializer = BasicUserInfoSerializer(User.objects.all(), many=True)
         return Response(serializer.data)
-
-
-class AuthTokenView(APIView):
-    """
-    Provides way of accessing authorization tokens just by providing username in the request.
-
-    This view exists only for testing purposes.
-    """
-
-    def post(self, request, username):
-        try:
-            user = User.objects.get(username=username)
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-        token, _ = Token.objects.get_or_create(user=user)
-        response = Response(data={'token': token.key})
-        response.set_cookie(SESSION_COOKIE_NAME, token.key, expires=SESSION_COOKIE_AGE)
-        return response
 
 
 class WhoAmIView(APIView):
