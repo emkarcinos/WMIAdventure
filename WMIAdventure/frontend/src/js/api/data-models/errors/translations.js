aa9103c828c346e75bd167df527aebed402d08be
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-18 00:09:29 +0100

    #820 Show validation errors at registration

--- WMIAdventure/frontend/src/js/api/data-models/errors/translations.js
+++ WMIAdventure/frontend/src/js/api/data-models/errors/translations.js
@@ -1,3 +1,7 @@
 export const errorTranslations = {
-    "This field may not be blank.": "To pole nie może być puste",
+    "This field may not be blank.": "To pole nie może być puste.",
+    "A user with that username already exists.": "Ta nazwa jest już zajęta.",
+    "This password is entirely numeric.": "Hasło nie może się składać z samych cyfr.",
+    "This password is too common.": "Hasło jest zbyt proste.",
+    "Password fields didn't match.": "Wprowadzone hasła nie są jednakowe"
 };
--- WMIAdventure/frontend/src/js/components/auth/organisms/UserRegistration/UserRegistration.js
+++ WMIAdventure/frontend/src/js/components/auth/organisms/UserRegistration/UserRegistration.js
@@ -3,13 +3,17 @@ import AuthForm from "../../molecules/AuthForm";
 import UsersAPIGateway from "../../../../api/gateways/UsersAPIGateway";
 import {Redirect} from "react-router-dom";
 import MainContainer from "./styled-components/MainContainer";
+import {concatMessage, translateErrors} from "../../../../api/data-models/errors/errors";
 
 class UserRegistration extends React.Component {
     state = {
         username: null,
         password: null,
         password2: null,
-        hasRegistered: false
+        hasRegistered: false,
+        usernameError: null,
+        passwordError: null,
+        password2Error: null,
     }
     onRegistrationSuccess = () => {
         UsersAPIGateway.login(this.state.username, this.state.password)
@@ -17,16 +21,30 @@ class UserRegistration extends React.Component {
                 resp.ok ? this.setState({hasRegistered: true}) : null;
             });
     }
+
+    onRegistrationFailure = (response) => {
+        const asyncHandle = async (response) => {
+            const data = await response.json()
+            const translatedError = translateErrors(data);
+            if (translatedError.username)
+                this.setState({usernameError: concatMessage(translatedError.username)})
+            if (translatedError.password)
+                this.setState({passwordError: concatMessage(translatedError.password)})
+            if (translatedError.password2)
+                this.setState({password2Error: concatMessage(translatedError.password2)})
+        }
+        asyncHandle(response);
+    }
+
+    clearErrors = () => {
+        this.setState({usernameError: null, passwordError: null, password2Error: null});
+    }
+
     onRegistrationFormSubmit = (event) => {
         event.preventDefault();
+        this.clearErrors()
         UsersAPIGateway.registerUser(this.state)
-            .then(resp => {
-                    resp.json().then(msg => alert(JSON.stringify(msg)
-                        .replace(/[{}"\]]+/g, '')
-                        .replace(/[,[]+/g, ' ')))
-                    resp.ok ? this.onRegistrationSuccess() : null;
-                }
-            );
+            .then(resp => resp.ok ? this.onRegistrationSuccess() : this.onRegistrationFailure(resp));
     }
 
     fieldChangedHandler = (event, field) => {
@@ -48,6 +66,9 @@ class UserRegistration extends React.Component {
                           updateUsernameState={evt => this.fieldChangedHandler(evt, 'username')}
                           updatePasswordState={evt => this.fieldChangedHandler(evt, 'password')}
                           updatePassword2State={evt => this.fieldChangedHandler(evt, 'password2')}
+                          loginError={this.state.usernameError}
+                          passwordError={this.state.passwordError}
+                          password2Error={this.state.password2Error}
                 />
                 {
                     this.state.hasRegistered ? <Redirect to={'/main'}/> : null
