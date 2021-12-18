523056d0318e5c4cb0bd397dc3a8d95974d196da
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-18 10:14:08 +0100

    #820 Add missing translation

--- WMIAdventure/frontend/src/js/api/data-models/errors/translations.js
+++ WMIAdventure/frontend/src/js/api/data-models/errors/translations.js
@@ -3,5 +3,6 @@ export const errorTranslations = {
     "A user with that username already exists.": "Ta nazwa jest już zajęta.",
     "This password is entirely numeric.": "Hasło nie może się składać z samych cyfr.",
     "This password is too common.": "Hasło jest zbyt proste.",
-    "Password fields didn't match.": "Wprowadzone hasła nie są jednakowe"
+    "Password fields didn't match.": "Wprowadzone hasła nie są jednakowe",
+    "This field may not be null.": "To pole nie może być puste."
 };
