601dcd84323f8086def946baf9f5ec530966f42f
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-20 15:58:00 +0100

    #792 Call backend via HTTPS on deployments

--- WMIAdventure/frontend/src/js/api/endpoints/API_URL.js
+++ WMIAdventure/frontend/src/js/api/endpoints/API_URL.js
@@ -1,4 +1,12 @@
-const API_URL = 'http://' + process.env['REACT_APP_API_URL'] + '/api/';
+const getEnvProtocol = () => {
+    const http = 'http://';
+    const https = 'https://';
+    if (process.env['REACT_APP_HTTPS_BACKEND'])
+        return https;
+    return http;
+}
+
+const API_URL = getEnvProtocol() + process.env['REACT_APP_API_URL'] + '/api/';
 
 /**
  * Main API url.
