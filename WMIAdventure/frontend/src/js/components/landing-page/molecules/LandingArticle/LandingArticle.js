a6a2dd825e3d1874e1c631380202bc7fb80d1e13
Mateusz <wirus006@gmail.com>
2021-12-22 12:37:47 +0100

    link to repo #863

--- WMIAdventure/frontend/src/js/components/landing-page/molecules/LandingArticle/LandingArticle.js
+++ WMIAdventure/frontend/src/js/components/landing-page/molecules/LandingArticle/LandingArticle.js
@@ -5,6 +5,7 @@ import H3 from "./styled-components/H3";
 import Image from "./styled-components/Image";
 import Paragraph from "./styled-components/Paragraph";
 import Line from "./styled-components/Line";
+import RepoLink from "../../../../pages/LandingPage/styled-components/RepoLink";
 
 class LandingArticle extends React.Component {
     render() {
@@ -16,9 +17,23 @@ class LandingArticle extends React.Component {
                     </H3>
                     <Image src={this.props.image}/>
                 </Div>
-                <Paragraph>
-                    {this.props.paragraph}
-                </Paragraph>
+                {this.props.github ?
+                    <Paragraph>
+                        Jesteś ciekawy jak aplikacja jest zbudowana?
+                        Chciałbyś zaproponować lub wprowadzić własne zmiany?
+                        Nie ma problemu! WMI Adventure to projekt open source,
+                        który jest otwarty na wszelkie sugestie i rozwój.
+                        Zapraszamy na nasze&nbsp;
+                        <RepoLink target='_blank' href='https://github.com/emkarcinos/WMIAdventure'>
+                            repozytorium
+                        </RepoLink>
+                        &nbsp;na githubie po więcej informacji.
+                    </Paragraph> :
+                    <Paragraph>
+                        {this.props.paragraph}
+                    </Paragraph>
+                }
+
                 {
                     this.props.line ?
                         <Line/>
