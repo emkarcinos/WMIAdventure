f9287f3e598b523b8c34e6f46eee347b779d21f4
Marcin Kostrzewski <komar86311@gmail.com>
2021-12-24 00:20:44 +0100

    #812 Disallow more than 2 registers/day

--- WMIAdventure/backend/WMIAdventure_backend/users/views.py
+++ WMIAdventure/backend/WMIAdventure_backend/users/views.py
@@ -1,4 +1,5 @@
 from django.utils.decorators import method_decorator
+from ratelimit.core import get_usage, is_ratelimited
 from ratelimit.decorators import ratelimit
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
@@ -6,23 +7,26 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from WMIAdventure_backend.settings import SESSION_COOKIE_AGE, SESSION_COOKIE_NAME
+from utils.errors import registration_ratelimit_error
 from .models import User
 from .serializers import RegisterSerializer, BasicUserInfoSerializer, BasicUserSerializer, LoginSerializer
 # Create your views here.
 from .signals import user_registered
 
-
 class UserRegister(APIView):
-    @method_decorator(ratelimit(key='ip', rate='2/d', method='POST'))
     def post(self, request):
         """
         Register user.
         """
+        limiting = get_usage(request, fn=UserRegister, key='ip', rate='1/d', increment=False)
+        if limiting is not None and limiting.get('should_limit', False):
+            return Response(registration_ratelimit_error, status=status.HTTP_400_BAD_REQUEST)
 
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
             user_registered.send(sender=self.__class__, user=user)
+            get_usage(request, fn=UserRegister, key='ip', rate='1/d', increment=True)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
---
+++ WMIAdventure/backend/WMIAdventure_backend/utils/errors.py
@@ -0,0 +1,5 @@
+registration_ratelimit_error = {
+    "username": [
+        "Wykorzystałeś limit rejestracji. Spróbuj ponownie później."
+    ]
+}
