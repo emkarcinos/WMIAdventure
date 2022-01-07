6c076c60445db89333fb07cb7ed37fce7cdce3f7
Michal-Czekanski <michal.cz2202@gmail.com>
2022-01-07 20:42:20 +0100

    #922 Add skill points gaining

--- WMIAdventure/backend/WMIAdventure_backend/IngameUsers/signals.py
+++ WMIAdventure/backend/WMIAdventure_backend/IngameUsers/signals.py
@@ -1,5 +1,7 @@
 import random
 
+from IngameUsers.businesslogic.experience.Experience import Experience
+from IngameUsers.businesslogic.skill_points import calculate_skill_points_gain
 from IngameUsers.models import UserProfile, Semester, UserStats
 from WMIAdventure_backend import settings
 from users.models import User
@@ -8,13 +10,31 @@ from users.views import UserRegister
 from utils.test_data.create_example_data import get_or_create_example_deck_for_user
 
 
+def user_gained_exp(user_stats: UserStats, level_before: int, level_after: int) -> None:
+    """
+    This function is called after user gained exp.
+
+    :param user_stats: 
+    :param level_before: Level before exp gain.
+    :param level_after: Level after exp gain.
+    """
+
+    user_stats.skill_points += calculate_skill_points_gain(level_before, level_after)
+    user_stats.save()
+
+
 def user_should_gain_exp(user_stats: UserStats, exp_amount: int):
     """
     Gives exp to player.
     """
 
+    level_before = Experience(user_stats.exp).level
+
     user_stats.exp += exp_amount
     user_stats.save()
+
+    level_after = Experience(user_stats.exp).level
+    user_gained_exp(user_stats, level_before, level_after)
 
 
 def on_user_create(sender, user: settings.AUTH_USER_MODEL, **kwargs):
