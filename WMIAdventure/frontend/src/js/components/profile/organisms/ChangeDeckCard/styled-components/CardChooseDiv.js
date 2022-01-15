59f359704df6ccaff657f9313d1a52228e355f21
Marcin Kostrzewski <komar86311@gmail.com>
2022-01-15 17:01:18 +0100

    #1002 Set height for CardList in ChangeDeckCard

--- WMIAdventure/frontend/src/js/components/profile/organisms/ChangeDeckCard/styled-components/CardChooseDiv.js
+++ WMIAdventure/frontend/src/js/components/profile/organisms/ChangeDeckCard/styled-components/CardChooseDiv.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const CardChooseDiv = styled.div`
   width: 100%;
+  height: 100%;
   overflow-y: scroll;
   scrollbar-width: none;
   -ms-overflow-style: none; /* IE 10+ */
