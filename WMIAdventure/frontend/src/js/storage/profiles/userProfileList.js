6ae5c85f2c74f9299e820feacbcc6115fb20cb8b
Marcin Kostrzewski <komar86311@gmail.com>
2022-01-19 15:22:53 +0100

    Call backend only for current user page

--- WMIAdventure/frontend/src/js/storage/profiles/userProfileList.js
+++ WMIAdventure/frontend/src/js/storage/profiles/userProfileList.js
@@ -1,27 +1,43 @@
-import {getWithSetCallback, set} from "../cache/cache";
+import {get, getWithSetCallback, set} from "../cache/cache";
 import {profileKey, userProfileKeys} from "../localStorageKeys";
 import UserProfilesAPIGateway from "../../api/gateways/UserProfilesAPIGateway";
 
 const cacheUsersForSeconds = 300 // 5 minutes;
 
 export const getAllUserProfiles = async (flatten = true) => {
-    const callback = async () => {
-        try {
-            const response = await UserProfilesAPIGateway.getAllBasicUsersInfo();
-            return await response;
-        } catch (errors) {
-            console.log(errors);
-            return null
-        }
+    let pageNumber = 1;
+    let page = await getUserListPage(pageNumber);
+    const results = [page];
+    while (page.hasNext) {
+        pageNumber++;
+        page = await getUserListPage(pageNumber)
+        results.push(page);
     }
-
-    const cachePages = await getWithSetCallback(userProfileKeys.profileList, callback, cacheUsersForSeconds);
-    return flatten ? cachePages.flatMap(item => item.results) : cachePages;
+    return flatten ? results.flatMap(item => item.results) : results;
 }
 
 export const getUserListPage = async (pageNumber) => {
-    const allProfiles = await getAllUserProfiles(false);
-    return allProfiles.filter(page => page.page === pageNumber)[0];
+    const callback = async () => {
+        try {
+            return await UserProfilesAPIGateway.getBasicUserInfoPage(pageNumber);
+        } catch (errors) {
+            console.log(errors);
+            return null;
+        }
+    }
+    const allUsersPaginatedCache = await get(userProfileKeys.profileList);
+    const allUsersPaginated = allUsersPaginatedCache ? allUsersPaginatedCache : [];
+    const pageFromCache = allUsersPaginated ? allUsersPaginated.filter(page => page.page === pageNumber)[0] : null;
+    if (pageFromCache)
+        return pageFromCache;
+
+    const pageFromBackend = await callback();
+    if (!pageFromBackend)
+        return null;
+    allUsersPaginated.push(pageFromBackend);
+    await set(userProfileKeys.profileList, allUsersPaginated, cacheUsersForSeconds);
+    return pageFromBackend;
+
 }
 
 export const getUserById = async (id, forceUpdate = true) => {
